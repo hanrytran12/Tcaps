e52f009109d393cb52a11341aa12aa9cd04ee8d9
Trần Võ Đức Duy <tranduy1632004@gmail.com>
2025-10-19 01:21:02 +0700

    GetProfile Staff

---
+++ Infrastructure/Repositories/AssignmentRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class AssignmentRepository : IAssignmentRepository
+    {
+        private readonly AppDbContext _context;
+
+        public AssignmentRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Assignment>> GetAssignmentsAsync(Guid workshopId)
+        {
+            return await _context.Assignments
+                .Where(a => a.WorkshopId == workshopId)
+                .ToListAsync();
+        }
+    }
+}
