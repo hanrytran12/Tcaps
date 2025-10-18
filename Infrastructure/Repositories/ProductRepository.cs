4fc2f105275f5d038ee8698433b86fdad42b0764
huy <sliverzack1@gmail.com>
2025-10-18 18:31:01 +0700

    chore(persistence): Implement Unit of Work and Repository patterns

---
+++ Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _context;
+        public ProductRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Product product)
+        {
+            await _context.Products.AddAsync(product);
+        }
+
+        public void Delete(Product product)
+        {
+            _context.Products.Remove(product);
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product?> GetByIdAsync(Guid id)
+        {
+            return await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<bool> IsCodeUniqueAsync(string code)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.Code == code);
+            return (product is null);
+        }
+
+        public void Update(Product product)
+        {
+            _context.Products.Update(product);
+        }
+    }
+}
