9d05de87ff008d3ae49a9bc624b7d69918b26550
huy <sliverzack1@gmail.com>
2025-10-18 18:29:35 +0700

    chore(core): Introduce Result pattern and custom exceptions

---
+++ Application/Common/Result.cs
@@ -0,0 +1,51 @@
+﻿namespace Application.Common
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess;
+        public T? Value { get; }
+        public string? Error { get; }
+
+        private Result(bool isSuccess, T? value, string? error)
+        {
+            if (isSuccess && error is not null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (!isSuccess && error is null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            IsSuccess = isSuccess;
+            Value = value;
+            Error = error;
+        }
+
+        public static Result<T> Success(T value) => new Result<T>(true, value, null);
+        public static Result<T> Failure(string error) => new Result<T>(false, default, error);
+    }
+
+    public class Result
+    {
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess;
+        public string? error { get; }
+
+        private Result(bool isSuccess, string? error)
+        {
+            if (isSuccess && error is not null)
+                throw new InvalidOperationException();
+            if (!isSuccess && error is null)
+                throw new InvalidOperationException();
+
+            IsSuccess = isSuccess;
+            this.error = error;
+        }
+
+        public static Result Success() => new Result(true, null);
+        public static Result Failure(string error) => new Result(false, error);
+    }
+}
