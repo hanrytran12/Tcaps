4c78045f1552621219e1835ac74c619e6ce88530
hanrytran12 <85053963+hanrytran12@users.noreply.github.com>
2025-10-19 13:57:49 +0700

    Feature/KAN-16 Implement User Account Management CRUD
    * feat(user): Implement Add, Update, and Delete for Users
    
    * fix(merge): Resolve conflicts from main branch

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,24 +1,51 @@
-﻿using Application.Interfaces;
+﻿using Application.Commands.AddUser;
+using Application.Commands.DeleteUser;
+using Application.Commands.UpdateUser;
+using Application.Queries.GetAllUser;
+using Domain.Entities;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [Route("api/user")]
     [ApiController]
+    [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
-        private readonly IStaffService _staffService;
-
-        public UserController(IStaffService staffService)
+        private readonly IMediator _mediator;
+        public UserController(IMediator mediator)
         {
-            _staffService = staffService;
+            _mediator = mediator;
         }
 
-        [HttpGet("{userId:guid}")]
-        public async Task<IActionResult> GetUserProfile(Guid userId)
+        [HttpGet]
+        public async Task<List<User>> GetAllUser()
         {
-            var response = await _staffService.GetUserProfileAsync(userId);
-            return StatusCode(response.StatusCode, response);
+            var listUser = await _mediator.Send(new GetAllUserQuery());
+            return listUser;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUser(AddUserCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return result.IsSuccess ? Ok(result.Value) : BadRequest(result.Error);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> UpdateUser(Guid id, [FromBody] UpdateUserCommand command)
+        {
+            command.Id = id;
+            var result = await _mediator.Send(command);
+            return result.IsSuccess ? NoContent() : BadRequest(result.error);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> DeleteUser(Guid id)
+        {
+            var command = new DeleteUserCommand(id);
+            var result = await _mediator.Send(command);
+            return result.IsSuccess ? NoContent() : BadRequest(result.error);
         }
     }
 }
---
+++ Application/Commands/DeleteUser/DeleteUserCommand.cs
@@ -0,0 +1,15 @@
+﻿using Application.Common;
+using MediatR;
+
+namespace Application.Commands.DeleteUser
+{
+    public class DeleteUserCommand : IRequest<Result>
+    {
+        public Guid Id { get; set; }
+
+        public DeleteUserCommand(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
