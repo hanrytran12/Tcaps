5271fe23b9e3d8d4483b98513e539241cb03adaf
hanrytran12 <85053963+hanrytran12@users.noreply.github.com>
2025-10-20 20:33:33 +0700

    feat(Batch): Implement Add, Update, Delete for Batch

---
+++ Infrastructure/Repositories/BatchRepository.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class BatchRepository : IBatchRepository
+    {
+        private readonly AppDbContext _context;
+        public BatchRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Batch batch)
+        {
+            await _context.AddAsync(batch);
+        }
+
+        public void Delete(Batch batch)
+        {
+            _context.Remove(batch);
+        }
+
+        public async Task<IEnumerable<Batch>> GetAllAsync()
+        {
+            return await _context.Batches.ToListAsync();
+        }
+
+        public async Task<Batch?> GetByIdAsync(Guid Id)
+        {
+            return await _context.Batches.FirstOrDefaultAsync(b => b.Id == Id);
+        }
+
+        public void Update(Batch batch)
+        {
+            _context.Update(batch);
+        }
+    }
+}
