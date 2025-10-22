1c9070d7fc0ee5b4223eebcc53b9f390c8049572
hanrytran12 <85053963+hanrytran12@users.noreply.github.com>
2025-10-22 23:51:14 +0700

    feat(notification): Implement notification API endpoints
    * feat(materialRequest): Notify admin on request approval
    
    * feat(notification): Implement notification API endpoints

---
+++ Application/Features/Notifications/Queries/GetNotifications/GetNotificationsQuery.cs
@@ -0,0 +1,16 @@
+﻿using Application.DTOs.Response;
+using MediatR;
+using System.Text.Json.Serialization;
+
+namespace Application.Features.Notifications.Queries.GetNotifications
+{
+    public class GetNotificationsQuery : IRequest<List<NotificationDTO>>
+    {
+        [JsonIgnore]
+        public Guid UserId { get; set; }
+
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 5;
+        public bool? UnreadOnly { get; set; }
+    }
+}
---
+++ Application/Features/Notifications/Queries/GetNotifications/GetNotificationsQueryHandler.cs
@@ -0,0 +1,44 @@
+﻿using Application.DTOs.Response;
+using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.Notifications.Queries.GetNotifications
+{
+    public class GetNotificationsQueryHandler : IRequestHandler<GetNotificationsQuery, List<NotificationDTO>>
+    {
+        private readonly IAppDbContext _context;
+        public GetNotificationsQueryHandler(IAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<NotificationDTO>> Handle(GetNotificationsQuery request, CancellationToken cancellationToken)
+        {
+            var query = _context.Notifications
+                .Where(n => n.UserId == request.UserId);
+
+            if (request.UnreadOnly == true)
+            {
+                query = query.Where(n => !n.IsRead);
+            }
+
+            var notifications = query
+                .OrderByDescending(n => n.CreatedAt)
+                .Skip((request.PageNumber - 1) * request.PageSize)
+                .Take(request.PageSize)
+                .Select(n => new NotificationDTO
+                {
+                    Id = n.Id,
+                    Title = n.Title,
+                    Message = n.Message,
+                    IsRead = n.IsRead,
+                    CreatedDate = n.CreatedAt
+                })
+                .AsNoTracking()
+                .ToListAsync();
+
+            return await notifications;
+        }
+    }
+}
