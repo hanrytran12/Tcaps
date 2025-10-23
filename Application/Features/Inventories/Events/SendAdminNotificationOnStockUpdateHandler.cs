15afd5d39e82f548fb058270f7dfbaa7bf068202
hanrytran12 <85053963+hanrytran12@users.noreply.github.com>
2025-10-23 18:25:52 +0700

    feat(notification): Send stock update notifications to admins
    * feat(materialRequest): Notify admin on request approval
    
    * feat(notification): Implement notification API endpoints
    
    * feat(Inventory): Implement Add for Inventory
    
    * feat(material): Update stock quantity via domain event on inventory creation
    
    * feat(notification): Send stock update notifications to admins

---
+++ Application/Features/Inventories/Events/SendAdminNotificationOnStockUpdateHandler.cs
@@ -0,0 +1,20 @@
+﻿using Application.Interfaces;
+using Domain.Events;
+using MediatR;
+
+namespace Application.Features.Inventories.Events
+{
+    public class SendAdminNotificationOnStockUpdateHandler : INotificationHandler<MaterialStockUpdatedEvent>
+    {
+        private readonly INotificationService _notificationService;
+        public SendAdminNotificationOnStockUpdateHandler(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        public async Task Handle(MaterialStockUpdatedEvent notification, CancellationToken cancellationToken)
+        {
+            await _notificationService.SendStockUpdateNotificationToAdminAsync(notification.MaterialName, notification.NewStockQuantity, notification.QuantityChange);
+        }
+    }
+}
