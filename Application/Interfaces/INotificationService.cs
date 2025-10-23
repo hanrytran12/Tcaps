cf938ccbf41c0f3f98867beb1cf338de23078d47
Trần Võ Đức Duy <tranduy1632004@gmail.com>
2025-10-23 23:46:53 +0700

    Xóa

--- Application/Interfaces/INotificationService.cs
+++ Application/Interfaces/INotificationService.cs
@@ -8,7 +8,6 @@ namespace Application.Interfaces
         Task SendMaterialRequestApprovalNotificationAsync(Guid materialId, Guid batchId, decimal quantityRequest);
         Task<ResponseDTO> GetNotificationByUserIdAsync(Guid userId, int pageNumber, int pageSize);
         Task SendStockUpdateNotificationToAdminAsync(string name, int newStockQuantity, int stockChange);
-        Task<ResponseDTO> GetNotificationByUserIdAsync(Guid userId);
         Task<ResponseDTO> MarkAsReadAsync(Guid notificationId);
         Task<ResponseDTO> CountNotificationAsync(Guid userId);
     }
