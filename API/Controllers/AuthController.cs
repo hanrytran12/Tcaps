06f918a1c61128849279b365ecc74d0b1270d5c2
huy <sliverzack1@gmail.com>
2025-10-25 18:39:00 +0700

    feat(auth): Implement JWT, Login for auth

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Application.Features.Auth.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> LoginAsync([FromQuery] LoginQuery query)
+        {
+            var result = await _mediator.Send(query);
+            if (result.IsFailure)
+            {
+                return BadRequest(result.Error);
+            }
+
+            return Ok(result.Value);
+        }
+    }
+}
---
+++ Application/Interfaces/IJwtTokenGenerator.cs
@@ -0,0 +1,10 @@
+﻿using Application.DTOs.Response;
+using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IJwtTokenGenerator
+    {
+        AuthRepsponseDTO GenerateToken(User user);
+    }
+}
