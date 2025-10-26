302e8860a628abd5416a8bea9a918e3b9d67cb94
huy <sliverzack1@gmail.com>
2025-10-26 14:52:08 +0700

    feat(Product): Implement image when add product is required

--- Application/Features/Products/Commands/AddProduct/AddProductCommand.cs
+++ Application/Features/Products/Commands/AddProduct/AddProductCommand.cs
@@ -9,6 +9,6 @@ namespace Application.Features.Products.Commands.AddProduct
         public string Code { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        public IFormFile? ImageFile { get; set; }
+        public IFormFile ImageFile { get; set; }
     }
 }
