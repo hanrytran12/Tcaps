cc42fbd183a659c1010dd3bac248b6684c227816
hanrytran12 <85053963+hanrytran12@users.noreply.github.com>
2025-10-27 23:26:45 +0700

    feat(Assignment): Update attribute UnitPrice for Assigments

--- Domain/Entities/Assignment.cs
+++ Domain/Entities/Assignment.cs
@@ -7,13 +7,14 @@ namespace Domain.Entities
         public Guid BatchId { get; private set; }
         public Guid WorkshopId { get; private set; }
         public int Quantity { get; private set; }
+        public decimal UnitPrice { get; private set; }
         public DateOnly StartDate { get; private set; }
         public DateOnly EndDate { get; private set; }
         public DateOnly ExpectedDeliveryDate { get; private set; }
         public string Status { get; private set; } = string.Empty;
         public DateOnly CreatedAt { get; private set; }
 
-        public Assignment(Guid id, Guid batchId, Guid workshopId, int quantity, DateOnly startDate, DateOnly endDate, DateOnly expectedDeliveryDate)
+        public Assignment(Guid id, Guid batchId, Guid workshopId, int quantity, DateOnly startDate, DateOnly endDate, DateOnly expectedDeliveryDate, decimal unitPrice)
             : base(id)
         {
             BatchId = batchId;
@@ -24,13 +25,14 @@ namespace Domain.Entities
             Status = "Pending";
             CreatedAt = DateOnly.FromDateTime(DateTime.Now);
             ExpectedDeliveryDate = expectedDeliveryDate;
+            UnitPrice = unitPrice;
         }
 
         private Assignment() : base(Guid.NewGuid()) { }
 
-        public static Assignment Create(Guid batchId, Guid workshopId, int quantity, DateOnly startDate, DateOnly endDate, DateOnly expectedDeliveryDate)
+        public static Assignment Create(Guid batchId, Guid workshopId, int quantity, DateOnly startDate, DateOnly endDate, DateOnly expectedDeliveryDate, decimal unitPrice)
         {
-            return new Assignment(Guid.NewGuid(), batchId, workshopId, quantity, startDate, endDate, expectedDeliveryDate);
+            return new Assignment(Guid.NewGuid(), batchId, workshopId, quantity, startDate, endDate, expectedDeliveryDate, unitPrice);
         }
 
         internal void UpdateStatus(string status)
