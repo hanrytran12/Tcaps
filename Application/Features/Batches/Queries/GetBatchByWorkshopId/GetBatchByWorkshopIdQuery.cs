c94b8a8e788c7542bdbc267d3d919c477bd7188e
huy <sliverzack1@gmail.com>
2025-10-31 16:53:24 +0700

    feat(QC): QC view batch list on their workshop

--- Application/Features/Batches/Queries/GetBatchByWorkshopId/GetBatchByWorkshopIdQuery.cs
+++ Application/Features/Batches/Queries/GetBatchByWorkshopId/GetBatchByWorkshopIdQuery.cs
@@ -1,14 +1,23 @@
-﻿using Domain.Entities;
+﻿using Application.DTOs.Response;
 using MediatR;
+using System.Text.Json.Serialization;
 
 namespace Application.Features.Batches.Queries.GetBatchByWorkshopId
 {
-    public class GetBatchByWorkshopIdQuery : IRequest<List<Batch>>
+    public class GetBatchByWorkshopIdQuery : IRequest<List<BatchDTO>>
     {
-        public Guid WorkshopId { get; set; }
-
+        [JsonIgnore]
+        public Guid UserId { get; set; }
         public string? Status { get; set; }
-        public DateOnly FromDate { get; set; }
-        public DateOnly ToDate { get; set; }
+        public DateOnly? FromDate { get; set; }
+        public DateOnly? ToDate { get; set; }
+
+        public GetBatchByWorkshopIdQuery(Guid userId, string? status, DateOnly? fromDate, DateOnly? toDate)
+        {
+            UserId = userId;
+            Status = status;
+            FromDate = fromDate;
+            ToDate = toDate;
+        }
     }
 }
