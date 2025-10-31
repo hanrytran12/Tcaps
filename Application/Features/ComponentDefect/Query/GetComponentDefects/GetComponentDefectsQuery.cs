3065918a566be52701d0edb4203d20ad9c57e747
hanrytran12 <85053963+hanrytran12@users.noreply.github.com>
2025-10-31 15:26:27 +0700

    feat(qc): allow tracking of rework requests

--- Application/Features/ComponentDefect/Query/GetComponentDefects/GetComponentDefectsQuery.cs
+++ Application/Features/ComponentDefect/Query/GetComponentDefects/GetComponentDefectsQuery.cs
@@ -1,10 +1,13 @@
 ﻿using Application.DTOs.Response;
 using MediatR;
+using System.Text.Json.Serialization;
 
 namespace Application.Features.ComponentDefects.Query.GetComponentDefects
 {
     public class GetComponentDefectsQuery : IRequest<List<ComponentDefectsDTO>>
     {
+        [JsonIgnore]
+        public Guid QCId { get; set; }
         public string? Status { get; set; }
     }
 }
