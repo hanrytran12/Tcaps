3f6b22358946f00c20713276392089b5fc4399a5
huy <sliverzack1@gmail.com>
2025-10-31 22:07:18 +0700

    feat(Assignment): Change status to "Ready" or "InProgress" for assignment

--- Domain/Entities/Assignment.cs
+++ Domain/Entities/Assignment.cs
@@ -41,5 +41,18 @@ namespace Domain.Entities
         {
             Status = status;
         }
+
+        public void UpdateWhenQcConfrimed()
+        {
+            if (StepOrder == 1)
+            {
+                UpdateStatus("InProgress");
+            }
+
+            else if (StepOrder > 1)
+            {
+                UpdateStatus("Ready");
+            }
+        }
     }
 }
