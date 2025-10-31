3aecfffefd34cf9b2f718a53197a80a62fcdde92
hanrytran12 <85053963+hanrytran12@users.noreply.github.com>
2025-10-31 16:53:52 +0700

    feat(QC): QC view batch list on their workshop

--- Application/Features/Batches/Queries/GetBatchByWorkshopId/GetBatchByWorkshopIdQueryHandler.cs
+++ Application/Features/Batches/Queries/GetBatchByWorkshopId/GetBatchByWorkshopIdQueryHandler.cs
@@ -1,44 +1,58 @@
-﻿using Application.Interfaces;
-using Domain.Entities;
+﻿using Application.DTOs.Response;
+using Application.Interfaces;
+using Domain.Interfaces;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace Application.Features.Batches.Queries.GetBatchByWorkshopId
 {
-    public class GetBatchByWorkshopIdQueryHandler : IRequestHandler<GetBatchByWorkshopIdQuery, List<Batch>>
+    public class GetBatchByWorkshopIdQueryHandler : IRequestHandler<GetBatchByWorkshopIdQuery, List<BatchDTO>>
     {
         private readonly IAppDbContext _context;
-        public GetBatchByWorkshopIdQueryHandler(IAppDbContext context)
+        private readonly IUserRepository _userRepository;
+
+        public GetBatchByWorkshopIdQueryHandler(IAppDbContext context, IUserRepository userRepository)
         {
             _context = context;
+            _userRepository = userRepository;
         }
 
-        public async Task<List<Batch>> Handle(GetBatchByWorkshopIdQuery request, CancellationToken cancellationToken)
+        public async Task<List<BatchDTO>> Handle(GetBatchByWorkshopIdQuery request, CancellationToken cancellationToken)
         {
-            //var originalList = new List<Batch>();
-            //var batches = await _batchRepository.GetAllWithAssignmentsAsync();
-            //foreach (var batch in batches)
-            //{
-            //    var assignments = batch.Assignments.FirstOrDefault(x => x.WorkshopId == request.WorkshopId);
-            //    if (assignments is not null)
-            //    {
-            //        originalList.Add(batch);
-            //    }
-            //}
+            var user = await _userRepository.GetByIdAsync(request.UserId);
 
-            //var filterList = originalList.Where(x => x.Status == request.Status);
-            //return (request.Status is null ? originalList.ToList() : filterList.ToList());
-
-            var query = _context.Batches.AsQueryable();
-
-            query = query.Where(batch => batch.Assignments.Any(assigment => assigment.WorkshopId == request.WorkshopId));
+            var query = from assignment in _context.Assignments
+                        where assignment.WorkshopId == user.WorkshopId
+                        join batch in _context.Batches on assignment.BatchId equals batch.Id
+                        join product in _context.Products on batch.ProductId equals product.Id
+                        select new { batch, product };
 
             if (!string.IsNullOrEmpty(request.Status))
             {
-                query = query.Where(batch => batch.Status == request.Status);
+                query = query.Where(batch => batch.batch.Status == request.Status);
             }
 
-            return await query.AsNoTracking().ToListAsync();
+            if (request.FromDate.HasValue)
+            {
+                query = query.Where(x => x.batch.StartDate >= request.FromDate.Value);
+            }
+
+            if (request.ToDate.HasValue)
+            {
+                query = query.Where(x => x.batch.EndDate <= request.ToDate.Value);
+            }
+
+            var finalQuery = query.Select(batch => new BatchDTO
+            {
+                ProductName = batch.product.Name,
+                Code = batch.batch.Code,
+                Quantity = batch.batch.Quantity,
+                StartDate = batch.batch.StartDate,
+                EndDate = batch.batch.EndDate,
+                Status = batch.batch.Status,
+            });
+
+            return await finalQuery.AsNoTracking().ToListAsync();
         }
     }
 }
