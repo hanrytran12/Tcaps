87993f7b68afab5eee26bf70b733eefa85e687fd
hanrytran12 <85053963+hanrytran12@users.noreply.github.com>
2025-11-02 14:58:33 +0700

    Feat/post many assignments in begin
    * feat(Workshop): Show list of workshop to create assignments
    
    * feat(Assignment): Implement Lead add all assignments in begin

---
+++ Application/Features/Workshop/Queries/GetWorkshopTemplate/GetWorkshopTemplateQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.DTOs.Response;
+using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.Workshop.Queries.GetWorkshopTemplate
+{
+    public class GetWorkshopTemplateQueryHandler : IRequestHandler<GetWorkshopTemplateQuery, List<WorkshopsDTO>>
+    {
+        private readonly IAppDbContext _appDbContext;
+        public GetWorkshopTemplateQueryHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<List<WorkshopsDTO>> Handle(GetWorkshopTemplateQuery request, CancellationToken cancellationToken)
+        {
+            return await _appDbContext.Workshop
+                .OrderBy(w => w.StepOrder)
+                .Select(w => new WorkshopsDTO
+                {
+                    WorkshopId = w.Id,
+                    WorkshopName = w.Name,
+                    StepOrder = w.StepOrder,
+                }).ToListAsync();
+        }
+    }
+}
