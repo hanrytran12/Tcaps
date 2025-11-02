38c619a625b9c8a682c0cbfb297516974b882914
huy <sliverzack1@gmail.com>
2025-11-02 10:21:36 +0700

    feat(Batch): Delete attribute Image from Batch

--- Domain/Entities/Batch.cs
+++ Domain/Entities/Batch.cs
@@ -8,7 +8,6 @@ namespace Domain.Entities
         public Guid ProductId { get; private set; }
         public string Code { get; private set; } = string.Empty;
         public decimal Quantity { get; private set; }
-        public string ImageURL { get; private set; }
         public DateOnly StartDate { get; private set; }
         public DateOnly EndDate { get; private set; }
         public DateOnly CreatedAt { get; private set; }
@@ -31,13 +30,12 @@ namespace Domain.Entities
 
         public ICollection<MaterialUse> MaterialUses { get; private set; } = new List<MaterialUse>();
 
-        public Batch(Guid Id, Guid productId, string code, decimal quantity, string imageURL, DateOnly startDate, DateOnly endDate)
+        public Batch(Guid Id, Guid productId, string code, decimal quantity, DateOnly startDate, DateOnly endDate)
             : base(Id)
         {
             ProductId = productId;
             Code = code;
             Quantity = quantity;
-            ImageURL = imageURL;
             StartDate = startDate;
             EndDate = endDate;
             CreatedAt = DateOnly.FromDateTime(DateTime.Now);
@@ -46,9 +44,9 @@ namespace Domain.Entities
 
         private Batch() : base(Guid.NewGuid()) { }
 
-        public static Batch Create(Guid productId, string code, decimal quantity, string imageURL, DateOnly startDate, DateOnly endDate)
+        public static Batch Create(Guid productId, string code, decimal quantity, DateOnly startDate, DateOnly endDate)
         {
-            return new Batch(Guid.NewGuid(), productId, code, quantity, imageURL, startDate, endDate);
+            return new Batch(Guid.NewGuid(), productId, code, quantity, startDate, endDate);
         }
 
         public void MarkAsDeleted()
