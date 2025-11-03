88cb38645e19a9bdc557432230bd606067e771dc
hanrytran12 <85053963+hanrytran12@users.noreply.github.com>
2025-11-03 16:48:12 +0700

    Refactor/lead create material request for qc
    * refactor(MaterialRequest): Delete old code
    
    * feat(MaterialRequest): Lead can add many material to assignment

---
+++ Application/Features/MaterialRequest/Commands/DispatchRequest/DispatchRequestCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using Application.Common;
+using Domain.Interfaces;
+using MediatR;
+
+namespace Application.Features.MaterialRequest.Commands.DispatchRequest
+{
+    public class DispatchRequestCommandHandler : IRequestHandler<DispatchRequestCommand, Result>
+    {
+        private readonly IBatchRepository _batchRepository;
+        private readonly IMaterialRequestRepository _materialRequestRepository;
+
+        public DispatchRequestCommandHandler(IBatchRepository batchRepository, IMaterialRequestRepository materialRequestRepository)
+        {
+            _batchRepository = batchRepository;
+            _materialRequestRepository = materialRequestRepository;
+        }
+
+        public async Task<Result> Handle(DispatchRequestCommand request, CancellationToken cancellationToken)
+        {
+            var batch = await _batchRepository.GetByAssignmentIdAsync(request.AssignmentId);
+
+            if (batch is null)
+            {
+                return Result.Failure("Không tìm thấy lô hàng");
+            }
+
+            foreach (var item in request.Items)
+            {
+                var materialRequest = Domain.Entities.MaterialRequest.Create(item.MaterialId, request.UserId, batch.Id, request.AssignmentId, item.Quantity, "Yêu cầu xuất kho từ Lead");
+                await _materialRequestRepository.AddAsync(materialRequest);
+            }
+
+            return Result.Success();
+        }
+    }
+}
