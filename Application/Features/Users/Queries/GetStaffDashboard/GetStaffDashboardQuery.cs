5c55fcc3f6547ef32e78b79b852f9e211df00e40
huy <sliverzack1@gmail.com>
2025-11-03 15:53:15 +0700

    feat(Staff): Staff see their dashboard about production

---
+++ Application/Features/Users/Queries/GetStaffDashboard/GetStaffDashboardQuery.cs
@@ -0,0 +1,13 @@
+﻿using Application.Common;
+using Application.DTOs.Request;
+using MediatR;
+using System.Text.Json.Serialization;
+
+namespace Application.Features.Users.Queries.GetStaffDashboard
+{
+    public class GetStaffDashboardQuery : IRequest<Result<StaffDashboardDTO>>
+    {
+        [JsonIgnore]
+        public Guid StaffId { get; set; }
+    }
+}
