27e933b16e6338a72708f43b0342a3be1aeeee1f
hanrytran12 <85053963+hanrytran12@users.noreply.github.com>
2025-11-03 15:54:05 +0700

    feat(Staff): Staff see their dashboard about production

---
+++ Application/DTOs/Request/StaffDashboardDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.DTOs.Request
+{
+    public class StaffDashboardDTO
+    {
+        public int TodayProducton { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal EstimatedTodayIncome { get; set; }
+    }
+}
