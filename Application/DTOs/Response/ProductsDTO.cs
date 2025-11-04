ccb84d909b5550f57bcae95ad4f0d845e9b1398b
huy <sliverzack1@gmail.com>
2025-11-04 19:57:14 +0700

    feat(Product): Update id from Product

--- Application/DTOs/Response/ProductsDTO.cs
+++ Application/DTOs/Response/ProductsDTO.cs
@@ -2,6 +2,7 @@
 {
     public class ProductsDTO
     {
+        public Guid ProductId { get; set; }
         public string Code { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
         public string Image { get; set; } = string.Empty;
