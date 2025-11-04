859dc0487e7b02c49a3da7c72b02627c453b4092
hanrytran12 <85053963+hanrytran12@users.noreply.github.com>
2025-11-05 00:05:14 +0700

    feat(MaterialRequest): QC get list request send by Lead about materials

---
+++ Application/DTOs/Response/PendingRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.DTOs.Response
+{
+    public class PendingRequestDTO
+    {
+        public Guid RequestId { get; set; }
+        public string MaterialName { get; set; } = string.Empty;
+        public decimal QuantityRequest { get; set; }
+        public string BatchCode { get; set; } = string.Empty;
+    }
+}
