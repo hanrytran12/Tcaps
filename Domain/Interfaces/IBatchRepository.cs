e9c62e1cbc744f59eb2197f2e13d80d5e5e28838
huy <sliverzack1@gmail.com>
2025-11-04 14:39:53 +0700

    feat(Batch): Get Batch by AssignmentId include MaterialUse

--- Domain/Interfaces/IBatchRepository.cs
+++ Domain/Interfaces/IBatchRepository.cs
@@ -16,6 +16,5 @@ namespace Domain.Interfaces
         Task<bool> AreAllAssignmentsCompletedAsync(Guid batchId);
         Task<Batch?> GetByAssignmentIdAsync(Guid assignmentId);
         Task<int?> GetLastCodeIndexAsync(string prefix);
-        Task<Batch?> GetByIdAssignmentWithMaterialUse(Guid assignmentId);
     }
 }
--- Infrastructure/Repositories/BatchRepository.cs
+++ Infrastructure/Repositories/BatchRepository.cs
@@ -98,5 +98,10 @@ namespace Infrastructure.Repositories
 
             return null;
         }
+
+        public async Task<Batch?> GetByIdAssignmentWithMaterialUse(Guid assignmentId)
+        {
+            return await _context.Batches.Include(b => b.Assignments).Include(batch => batch.MaterialUses).FirstOrDefaultAsync(b => b.Assignments.Any(a => a.Id == assignmentId));
+        }
     }
 }
