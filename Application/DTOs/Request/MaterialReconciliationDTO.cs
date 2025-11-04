6b3d239733c08236c6c71aed2edaacb342f54e6b
huy <sliverzack1@gmail.com>
2025-11-04 14:34:24 +0700

    feat(TransferRequest): QC enter reconciledQuantity to create request

---
+++ Application/DTOs/Request/MaterialReconciliationDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.DTOs.Request
+{
+    public class MaterialReconciliationDTO
+    {
+        public Guid MaterialId { get; set; }
+        public decimal ReconciliationQuantity { get; set; }
+    }
+}
