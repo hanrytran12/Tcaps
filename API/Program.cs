78aebd59b08e6dab8284f0b548938a4b21977070
huy <sliverzack1@gmail.com>
2025-11-04 14:39:53 +0700

    feat(Batch): Get Batch by AssignmentId include MaterialUse

--- API/Program.cs
+++ API/Program.cs
@@ -5,7 +5,6 @@ using Application.Features.Assignments.Commands.PlanAssignments;
 using Application.Features.Assignments.Queries;
 using Application.Features.AssingmentTransferRequest.Commands.AddAssignmenTransferRequest;
 using Application.Features.AssingmentTransferRequest.Commands.UpdateAssignmentTransferRequest;
-using Application.Features.AssingmentTransferRequest.Queries.GetReconciliationSummary;
 using Application.Features.Auth.Queries;
 using Application.Features.Batches.Commands.AddBatch;
 using Application.Features.Batches.Commands.DeleteBatch;
@@ -190,9 +189,7 @@ builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(Pro
                                                                       typeof(AddAssignmentTransferRequestCommand).Assembly,
                                                                       typeof(UpdateAssignmentTransferRequestCommand).Assembly,
 
-                                                                      typeof(GetWorkshopTemplateQuery).Assembly,
-
-                                                                      typeof(GetReconciliationSummaryQuery).Assembly
+                                                                      typeof(GetWorkshopTemplateQuery).Assembly
                                                                       ));
 
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
