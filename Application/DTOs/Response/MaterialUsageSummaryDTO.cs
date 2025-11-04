70ce5398c4852951191a257be6956e19187aa974
huy <sliverzack1@gmail.com>
2025-11-04 14:41:20 +0700

    Reapply "feat(TransferRequest): QC view dashboard before create request"
    This reverts commit ec6f931acbd8f3864b6e6c60adce7f97343b7d1a.

---
+++ Application/DTOs/Response/MaterialUsageSummaryDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.DTOs.Response
+{
+    public class MaterialUsageSummaryDTO
+    {
+        public Guid MaterialId { get; set; }
+        public string MaterialName { get; set; } = string.Empty;
+        public decimal QuantityDivided { get; set; }
+        public decimal QuantityStaffUsed { get; set; }
+    }
+}
