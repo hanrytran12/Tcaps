cec253147dfdd2f4dd9a6de8a7107376a9213f7b
Trần Võ Đức Duy <tranduy1632004@gmail.com>
2025-11-06 17:51:00 +0700

    Get user profile

--- Application/DTOs/Response/UserDTO.cs
+++ Application/DTOs/Response/UserDTO.cs
@@ -8,8 +8,12 @@ namespace Application.DTOs.Response
 {
     public class UserDTO
     {
+        public Guid WorkshopId { get; set; }
+        public string Role { get; set; } = string.Empty;
         public string FullName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
     }
 }
