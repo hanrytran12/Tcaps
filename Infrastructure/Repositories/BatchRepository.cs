d9fe9db92e1e70936d7e68367a48e8c7dd7d0d8b
huy <sliverzack1@gmail.com>
2025-11-07 23:55:46 +0700

    Update BatchRepository.cs

--- Infrastructure/Repositories/BatchRepository.cs
+++ Infrastructure/Repositories/BatchRepository.cs
@@ -25,7 +25,7 @@ namespace Infrastructure.Repositories
 
         public async Task<IEnumerable<Batch>> GetAllAsync()
         {
-            return await _context.Batches.Where(b => !b.isDeleted).ToListAsync();
+            return await _context.Batches.Include(b => b.Assignments).Where(b => !b.isDeleted).ToListAsync();
         }
 
         public async Task<Batch?> GetByIdAsync(Guid Id)
