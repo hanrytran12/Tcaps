5f3dec0f12a6758e00ef1990f191794ed34ea6df
huy <sliverzack1@gmail.com>
2025-11-08 20:46:07 +0700

    feat(TransferRequest): Get transfer request

---
+++ Application/Features/AssingmentTransferRequest/Queries/GetAllTransferRequest/GetAllTransferRequestQuery.cs
@@ -0,0 +1,9 @@
+﻿using Application.DTOs.Response;
+using MediatR;
+
+namespace Application.Features.AssingmentTransferRequest.Queries.GetAllTransferRequest
+{
+    public class GetAllTransferRequestQuery : IRequest<List<TrasnferRequestDTO>>
+    {
+    }
+}
