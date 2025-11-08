a33ca2de65c87b860b27c6c8200fe25845b59b51
hanrytran12 <85053963+hanrytran12@users.noreply.github.com>
2025-11-08 20:46:26 +0700

    feat(TransferRequest): Get transfer request

---
+++ Application/DTOs/Response/TrasnferRequestDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.DTOs.Response
+{
+    public class TrasnferRequestDTO
+    {
+        public string BatchCode { get; set; } = string.Empty;
+        public string WorkshopName { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public decimal CompletedQuantity { get; set; }
+        public string Note { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+    }
+}
