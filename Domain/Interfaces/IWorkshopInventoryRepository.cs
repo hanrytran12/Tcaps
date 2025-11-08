fc1104d53b679ac84e9802ee2196e31f9bfa17e1
huy <sliverzack1@gmail.com>
2025-11-08 17:54:30 +0700

    feat(MaterialUse): If quantity is exist on workshop inventory, add to material use

--- Domain/Interfaces/IWorkshopInventoryRepository.cs
+++ Domain/Interfaces/IWorkshopInventoryRepository.cs
@@ -4,6 +4,7 @@ namespace Domain.Interfaces
 {
     public interface IWorkshopInventoryRepository
     {
+        Task<WorkshopInventory?> GetByMaterialIdAndWorkshopIdAsync(Guid materialId, Guid workshopId);
         Task AddAsync(WorkshopInventory workshopInventory);
     }
 }
