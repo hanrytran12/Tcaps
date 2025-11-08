3d81ec0abdc33624be989b22f686ea64f2a444a0
huy <sliverzack1@gmail.com>
2025-11-08 20:03:57 +0700

    feat(MaterialRequest): Get all material request

---
+++ Application/Features/MaterialRequest/Queries/GetAllMaterialRequest/GetAllMaterialRequestQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.MaterialRequest.Queries.GetAllMaterialRequest
+{
+    public class GetAllMaterialRequestQueryHandler : IRequestHandler<GetAllMaterialRequestQuery, List<Domain.Entities.MaterialRequest>>
+    {
+        private readonly IAppDbContext _appDbContext;
+
+        public GetAllMaterialRequestQueryHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<List<Domain.Entities.MaterialRequest>> Handle(GetAllMaterialRequestQuery request, CancellationToken cancellationToken)
+        {
+            return await _appDbContext.MaterialRequests.AsNoTracking().ToListAsync(cancellationToken);
+        }
+    }
+}
