1ee47b994c2e4ec32d269e26128a178569affb18
hanrytran12 <85053963+hanrytran12@users.noreply.github.com>
2025-11-08 18:10:21 +0700

    feat(User): Get user by workshopId

---
+++ Application/Features/Users/Queries/GetUserByWorkshopId/GetUserByWorkshopIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Users.Queries.GetUserByWorkshopId
+{
+    public class GetUserByWorkshopIdQuery : IRequest<User>
+    {
+        public Guid WorkshopId { get; set; }
+
+        public GetUserByWorkshopIdQuery(Guid workshopId)
+        { WorkshopId = workshopId; }
+    }
+}
---
+++ Application/Features/Users/Queries/GetUserByWorkshopId/GetUserByWorkshopIdQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.Users.Queries.GetUserByWorkshopId
+{
+    public class GetUserByWorkshopIdQueryHandler : IRequestHandler<GetUserByWorkshopIdQuery, User>
+    {
+        private readonly IAppDbContext _appDbContext;
+        public GetUserByWorkshopIdQueryHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<User> Handle(GetUserByWorkshopIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _appDbContext.Users.AsNoTracking().Where(u => u.WorkshopId == request.WorkshopId && u.Role == "QC").FirstOrDefaultAsync(cancellationToken);
+        }
+    }
+}
