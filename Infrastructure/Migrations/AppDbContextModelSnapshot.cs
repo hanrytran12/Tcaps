92c09e5128bf159f751025985e2e8cae6aeeb0fb
huy <sliverzack1@gmail.com>
2025-11-09 13:45:23 +0700

    feat(Database): new database

--- Infrastructure/Migrations/AppDbContextModelSnapshot.cs
+++ Infrastructure/Migrations/AppDbContextModelSnapshot.cs
@@ -520,6 +520,39 @@ namespace Infrastructure.Migrations
                     b.ToTable("Productions");
                 });
 
+            modelBuilder.Entity("Domain.Entities.TaskTransferRequest", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("uniqueidentifier");
+
+                    b.Property<DateOnly?>("ApprovedAt")
+                        .HasColumnType("date");
+
+                    b.Property<Guid>("BatchId")
+                        .HasColumnType("uniqueidentifier");
+
+                    b.Property<DateOnly>("CreatedAt")
+                        .HasColumnType("date");
+
+                    b.Property<string>("Note")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<Guid>("QcTransportId")
+                        .HasColumnType("uniqueidentifier");
+
+                    b.Property<string>("Status")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<Guid>("WorkshopId")
+                        .HasColumnType("uniqueidentifier");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("TaskTransferRequests");
+                });
+
             modelBuilder.Entity("Domain.Entities.User", b =>
                 {
                     b.Property<Guid>("Id")
@@ -536,6 +569,9 @@ namespace Infrastructure.Migrations
                     b.Property<string>("FullName")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
+
+                    b.Property<bool>("IsQcTransport")
+                        .HasColumnType("bit");
 
                     b.Property<string>("PasswordHash")
                         .HasColumnType("nvarchar(max)");
