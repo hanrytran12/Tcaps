0c319f3f8e4a97d51d9d82941673f83f5ecad898
hanrytran12 <85053963+hanrytran12@users.noreply.github.com>
2025-11-10 00:05:24 +0700

    feat(ReworkRequest): Lead approve rework request from QC

---
+++ Application/Features/ReworkRequest/Commands/ApproveReworkRequest/ApproveReworkRequestCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.Common;
+using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.ReworkRequest.Commands.ApproveReworkRequest
+{
+    public class ApproveReworkRequestCommandHandler : IRequestHandler<ApproveReworkRequestCommand, Result>
+    {
+        private readonly IAppDbContext _appDbContext;
+
+        public ApproveReworkRequestCommandHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<Result> Handle(ApproveReworkRequestCommand request, CancellationToken cancellationToken)
+        {
+            var query = from rr in _appDbContext.ReworkRequests
+                        where rr.Id == request.RequestId && rr.Status == "PendingLead"
+                        join a in _appDbContext.Assignments on rr.AssignmentId equals a.Id
+                        select new { a, rr };
+
+            var queryInfo = await query.FirstOrDefaultAsync();
+
+            var assignment = queryInfo.a;
+            assignment.UpdateStatus("Reworking");
+
+            var reworkRequest = queryInfo.rr;
+            reworkRequest.ApproveRequest();
+
+            return Result.Success();
+        }
+    }
+}
