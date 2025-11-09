d17a5f9ea1da1e39e284f0d45ec06e51419c8069
hanrytran12 <85053963+hanrytran12@users.noreply.github.com>
2025-11-09 22:32:36 +0700

    feat(ReworkRequest): Create rework request from QC to Lead

---
+++ API/Controllers/ReworkRequestController.cs
@@ -0,0 +1,28 @@
+﻿using Application.Features.ReworkRequest.Commands.CreateReworkRequest;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReworkRequestController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ReworkRequestController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateReworkRequest([FromBody] CreateReworkRequestCommand command)
+        {
+            var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            command.QCId = userId;
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+    }
+}
---
+++ Application/Features/ReworkRequest/Commands/CreateReworkRequest/CreateReworkRequestCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using Application.Common;
+using Domain.Interfaces;
+using MediatR;
+
+namespace Application.Features.ReworkRequest.Commands.CreateReworkRequest
+{
+    public class CreateReworkRequestCommandHandler : IRequestHandler<CreateReworkRequestCommand, Result<Guid>>
+    {
+        private readonly IReworkRequestRepository _repository;
+
+        public CreateReworkRequestCommandHandler(IReworkRequestRepository reworkRequestRepository)
+        {
+            _repository = reworkRequestRepository;
+        }
+
+        public async Task<Result<Guid>> Handle(CreateReworkRequestCommand request, CancellationToken cancellationToken)
+        {
+            var reworkRequest = Domain.Entities.ReworkRequest.Create(request.QCId, request.AssigmentId, request.DefectiveQuantity, request.NoteQc);
+            await _repository.AddReworkRequestAsync(reworkRequest);
+            return Result<Guid>.Success(reworkRequest.Id);
+        }
+    }
+}
