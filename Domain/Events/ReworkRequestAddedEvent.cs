4e64adf22ac1576dac4f90b1feabb75807d68cf5
huy <sliverzack1@gmail.com>
2025-11-09 23:08:00 +0700

    feat(Notification): Send noti to Lead when QC create rework request

---
+++ Domain/Events/ReworkRequestAddedEvent.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+namespace Domain.Events
+{
+    public class ReworkRequestAddedEvent : INotification
+    {
+        public Guid AssignmentId { get; set; }
+        public Guid QcId { get; set; }
+        public decimal DefectiveQuantity { get; set; }
+        public string NoteQC { get; set; } = string.Empty;
+
+        public ReworkRequestAddedEvent(Guid assignmentId, Guid qcId, decimal defectiveQuantity, string noteQC)
+        {
+            AssignmentId = assignmentId;
+            QcId = qcId;
+            DefectiveQuantity = defectiveQuantity;
+            NoteQC = noteQC;
+        }
+    }
+}
