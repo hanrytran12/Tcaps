1f062fc0c551d9e5d405c8e44229834c2eed2be1
hanrytran12 <85053963+hanrytran12@users.noreply.github.com>
2025-11-09 22:32:36 +0700

    feat(ReworkRequest): Create rework request from QC to Lead

---
+++ Application/Features/ReworkRequest/Commands/CreateReworkRequest/CreateReworkRequestCommand.cs
@@ -0,0 +1,15 @@
+﻿using Application.Common;
+using MediatR;
+using System.Text.Json.Serialization;
+
+namespace Application.Features.ReworkRequest.Commands.CreateReworkRequest
+{
+    public class CreateReworkRequestCommand : IRequest<Result<Guid>>
+    {
+        public Guid AssigmentId { get; set; }
+        [JsonIgnore]
+        public Guid QCId { get; set; }
+        public decimal DefectiveQuantity { get; set; }
+        public string NoteQc { get; set; } = string.Empty;
+    }
+}
