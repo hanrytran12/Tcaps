7d96c28730cd8908dc4d0148f2265561dfc6bca8
huy <sliverzack1@gmail.com>
2025-11-10 16:23:19 +0700

    feat(Production): Add new attribute ReworkRequestID

---
+++ Infrastructure/Migrations/20251110092211_AddReworkRequestIDToProductions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReworkRequestIDToProductions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ReworkRequestId",
+                table: "Productions",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReworkRequestId",
+                table: "Productions");
+        }
+    }
+}
