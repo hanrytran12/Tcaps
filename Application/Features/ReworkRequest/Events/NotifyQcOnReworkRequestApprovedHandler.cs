0eba4447d31c693ce3a1dc5cf480df4488bbaf97
huy <sliverzack1@gmail.com>
2025-11-10 14:12:20 +0700

    feat(Notification): Send to QC when Lead approve rework request

---
+++ Application/Features/ReworkRequest/Events/NotifyQcOnReworkRequestApprovedHandler.cs
@@ -0,0 +1,33 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Domain.Events;
+using Domain.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.ReworkRequest.Events
+{
+    public class NotifyQcOnReworkRequestApprovedHandler : INotificationHandler<ReworkRequestApprovedEvent>
+    {
+        private readonly IAppDbContext _appDbContext;
+        private readonly INotificationRepository _notificationRepository;
+
+        public NotifyQcOnReworkRequestApprovedHandler(IAppDbContext appDbContext, INotificationRepository notificationRepository)
+        {
+            _appDbContext = appDbContext;
+            _notificationRepository = notificationRepository;
+        }
+
+        public async Task Handle(ReworkRequestApprovedEvent notification, CancellationToken cancellationToken)
+        {
+            var qcUser = await _appDbContext.Users.AsNoTracking().Where(u => u.Id == notification.QcId).FirstOrDefaultAsync(cancellationToken);
+
+            var type = "REWORK_REQUEST_APPROVED";
+            var title = "Yêu cầu làm lại sản phẩm lỗi";
+            var message = $"Xưởng bạn sẽ làm lại sản phẩm lỗi. Nhận nguyên vật liệu vào ngày {notification.DeliveryDate} và hoàn thành trước ngày {notification.EndDate}";
+
+            var noti = Notification.Create(qcUser.Id, title, message, type);
+            await _notificationRepository.AddAsync(noti);
+        }
+    }
+}
