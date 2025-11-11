aae81d2b144c667de3ace898aafca60741ea0633
Trần Võ Đức Duy <tranduy1632004@gmail.com>
2025-11-11 12:41:48 +0700

    Feat(Batches): Add batchId

--- Application/DTOs/Response/BatchDTO.cs
+++ Application/DTOs/Response/BatchDTO.cs
@@ -2,6 +2,7 @@
 {
     public class BatchDTO
     {
+        public Guid BatchId { get; set; }
         public string ProductName { get; set; } = string.Empty;
         public string Code { get; set; } = string.Empty;
         public decimal Quantity { get; set; }
