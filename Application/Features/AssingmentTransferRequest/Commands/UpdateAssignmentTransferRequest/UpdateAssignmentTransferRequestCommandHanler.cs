c1d69540b1bc71de781d639387a9623079a4489f
hanrytran12 <85053963+hanrytran12@users.noreply.github.com>
2025-11-11 19:31:16 +0700

    feat(TransferRequest): Lead approve transfer request for rework

--- Application/Features/AssingmentTransferRequest/Commands/UpdateAssignmentTransferRequest/UpdateAssignmentTransferRequestCommandHanler.cs
+++ Application/Features/AssingmentTransferRequest/Commands/UpdateAssignmentTransferRequest/UpdateAssignmentTransferRequestCommandHanler.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Common;
+using Application.Interfaces;
 using Domain.Interfaces;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Features.AssingmentTransferRequest.Commands.UpdateAssignmentTransferRequest
 {
@@ -9,37 +11,50 @@ namespace Application.Features.AssingmentTransferRequest.Commands.UpdateAssignme
         private readonly IAssignmentRepository _assignmentRepository;
         private readonly IAssignmentTransferRequestRepository _assignmentTransferRequestRepository;
         private readonly IBatchRepository _batchRepository;
+        private readonly IAppDbContext _appDbContext;
 
-        public UpdateAssignmentTransferRequestCommandHanler(IAssignmentRepository assignmentRepository, IAssignmentTransferRequestRepository assignmentTransferRequestRepository, IBatchRepository batchRepository)
+        public UpdateAssignmentTransferRequestCommandHanler(IAssignmentRepository assignmentRepository, IAssignmentTransferRequestRepository assignmentTransferRequestRepository, IBatchRepository batchRepository, IAppDbContext appDbContext)
         {
             _assignmentRepository = assignmentRepository;
             _assignmentTransferRequestRepository = assignmentTransferRequestRepository;
             _batchRepository = batchRepository;
+            _appDbContext = appDbContext;
         }
 
         public async Task<Result> Handle(UpdateAssignmentTransferRequestCommand request, CancellationToken cancellationToken)
         {
             var transferRequest = await _assignmentTransferRequestRepository.GetByIdAsync(request.TransferRequestId);
-            if (transferRequest is null || transferRequest.Status != "PendingApproval")
+            if (transferRequest.ReworkRequestId == null)
             {
-                return Result.Failure("Yêu cầu không hợp lệ hoặc đã được duyệt");
+                if (transferRequest is null || transferRequest.Status != "PendingApproval")
+                {
+                    return Result.Failure("Yêu cầu không hợp lệ hoặc đã được duyệt");
+                }
+
+                var assigment = await _assignmentRepository.GetByIdAsync(transferRequest.AssignmentId);
+                if (assigment is null)
+                {
+                    return Result.Failure("Không tìm thấy công đoạn");
+                }
+
+                var batch = await _batchRepository.GetByAssignmentIdAsync(assigment.Id);
+                if (batch is null)
+                {
+                    return Result.Failure("Không tìm thấy lô hàng");
+                }
+
+                batch.CompleteAndActiveNextAssignment(assigment.Id);
+            }
+            else
+            {
+                var reworkRequest = await _appDbContext.ReworkRequests.Where(r => r.Id == transferRequest.ReworkRequestId).FirstOrDefaultAsync();
+                reworkRequest.Completed();
+
+                var assignment = await _assignmentRepository.GetByIdAsync(reworkRequest.AssignmentId);
+                assignment.UpdateStatus("Completed");
             }
 
-            var assigment = await _assignmentRepository.GetByIdAsync(transferRequest.AssignmentId);
-            if (assigment is null)
-            {
-                return Result.Failure("Không tìm thấy công đoạn");
-            }
-
-            var batch = await _batchRepository.GetByAssignmentIdAsync(assigment.Id);
-            if (batch is null)
-            {
-                return Result.Failure("Không tìm thấy lô hàng");
-            }
-
-            batch.CompleteAndActiveNextAssignment(assigment.Id);
             transferRequest.MarkAsApproved();
-
             return Result.Success();
         }
     }
