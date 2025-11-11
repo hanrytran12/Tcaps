d136d0da5f69d5882e67dc4e1f2894ba69c49995
huy <sliverzack1@gmail.com>
2025-11-11 17:58:23 +0700

    ref(TransferRequest): If create transfer request for rework request, add rework request Id to know

--- Application/Services/AssignmentCompletionService.cs
+++ Application/Services/AssignmentCompletionService.cs
@@ -11,19 +11,34 @@ namespace Application.Services
             _appDbContext = appDbContext;
         }
 
-        public async Task<decimal> CalculateCompetedQuantityAsync(Guid assignmentId)
+        public async Task<decimal> CalculateCompetedQuantityAsync(Guid assignmentId, Guid? reworkRequestId)
         {
-            var totalQuantity = await _appDbContext.Productions.Where(p => p.AssignId == assignmentId).SumAsync(p => p.Quantity);
+            if (reworkRequestId == null)
+            {
+                var totalQuantity = await _appDbContext.Productions.Where(p => p.AssignId == assignmentId).SumAsync(p => p.Quantity);
 
-            var query = from p in _appDbContext.Productions
-                        where p.AssignId == assignmentId
-                        join e in _appDbContext.Evaluates on p.Id equals e.ProductionId
-                        where e.Status == "Rejected"
-                        select e.QuantityError;
+                var query = from p in _appDbContext.Productions
+                            where p.AssignId == assignmentId
+                            join e in _appDbContext.Evaluates on p.Id equals e.ProductionId
+                            where e.Status == "Rejected"
+                            select e.QuantityError;
 
-            var totalReject = await query.SumAsync();
+                var totalReject = await query.SumAsync();
+                return totalQuantity - totalReject;
+            }
+            else
+            {
+                var totalSubmitted = await _appDbContext.Productions.AsNoTracking().Where(p => p.AssignId == assignmentId && p.ReworkRequestId == reworkRequestId).SumAsync(p => p.Quantity);
 
-            return totalQuantity - totalReject;
+                var query = from p in _appDbContext.Productions
+                            where p.AssignId == assignmentId && p.ReworkRequestId == reworkRequestId
+                            join e in _appDbContext.Evaluates on p.Id equals e.ProductionId
+                            where e.Status == "Rejected"
+                            select e.QuantityError;
+
+                var totalRejected = await query.SumAsync();
+                return totalSubmitted - totalRejected;
+            }
         }
     }
 }
