afb4b12e33c633be9d13913c7725d795fb30ba6b
huy <sliverzack1@gmail.com>
2025-11-11 18:37:52 +0700

    feat(TrasnferRequest): add rework request id to transfer request

---
+++ Infrastructure/Migrations/20251111113431_UPDATE2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UPDATE2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ReworkRequestId",
+                table: "AssignmentTransferRequests",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReworkRequestId",
+                table: "AssignmentTransferRequests");
+        }
+    }
+}
