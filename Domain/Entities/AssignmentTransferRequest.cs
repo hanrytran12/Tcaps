cea8d74672cff340daf3032459160e1c7ab58958
hanrytran12 <85053963+hanrytran12@users.noreply.github.com>
2025-11-11 18:38:14 +0700

    feat(TrasnferRequest): add rework request id to transfer request

--- Domain/Entities/AssignmentTransferRequest.cs
+++ Domain/Entities/AssignmentTransferRequest.cs
@@ -5,13 +5,14 @@ namespace Domain.Entities
     public class AssignmentTransferRequest : AggregrateRoot
     {
         public Guid AssignmentId { get; private set; }
+        public Guid? ReworkRequestId { get; private set; }
         public Guid UserId { get; private set; }
         public decimal CompletedQuantity { get; private set; }
         public string Status { get; private set; } = string.Empty;
         public string? Note { get; private set; } = string.Empty;
         public DateOnly CreatedAt { get; private set; }
 
-        public AssignmentTransferRequest(Guid RequestId, Guid assignmentId, Guid userId, decimal completedQuantity, string? note)
+        public AssignmentTransferRequest(Guid RequestId, Guid assignmentId, Guid userId, decimal completedQuantity, string? note, Guid? reworkRequestId)
             : base(RequestId)
         {
             AssignmentId = assignmentId;
@@ -20,13 +21,14 @@ namespace Domain.Entities
             Status = "PendingApproval";
             Note = note;
             CreatedAt = DateOnly.FromDateTime(DateTime.UtcNow);
+            ReworkRequestId = reworkRequestId;
         }
 
         private AssignmentTransferRequest() : base(Guid.NewGuid()) { }
 
-        public static AssignmentTransferRequest Create(Guid assignmentId, Guid userId, decimal completedQuantity, string? note)
+        public static AssignmentTransferRequest Create(Guid assignmentId, Guid userId, decimal completedQuantity, string? note, Guid? reworkRequestId)
         {
-            var transferRequest = new AssignmentTransferRequest(Guid.NewGuid(), assignmentId, userId, completedQuantity, note);
+            var transferRequest = new AssignmentTransferRequest(Guid.NewGuid(), assignmentId, userId, completedQuantity, note, reworkRequestId);
             //transferRequest.AddDomainEvent(new TransferRequestAddedEvent(userId));
             return transferRequest;
         }
