f8ec4c7946aa505d9593e0ed413a9e3a66ef32e8
huy <sliverzack1@gmail.com>
2025-11-11 16:20:02 +0700

    feat(Database): Add new attribute "ReworkRequestId" for MaterialUse

--- Domain/Entities/MaterialUse.cs
+++ Domain/Entities/MaterialUse.cs
@@ -7,13 +7,14 @@ namespace Domain.Entities
         public Guid MaterialId { get; private set; }
         public Guid BatchId { get; private set; }
         public Guid AssignId { get; private set; }
+        public Guid? ReworkRequestId { get; private set; }
         public decimal QuantityDivide { get; private set; }
         public decimal QuantityStaffUse { get; private set; }
         public decimal ReconciledQuantity { get; private set; }
         public decimal QuantityRequest { get; private set; }
         public DateOnly Date { get; private set; }
 
-        public MaterialUse(Guid id, Guid materialId, Guid batchId, Guid assignId, decimal quantityDivide)
+        public MaterialUse(Guid id, Guid materialId, Guid batchId, Guid assignId, decimal quantityDivide, Guid? reworkRequestId)
             : base(id)
         {
             MaterialId = materialId;
@@ -21,13 +22,14 @@ namespace Domain.Entities
             AssignId = assignId;
             QuantityDivide = quantityDivide;
             Date = DateOnly.FromDateTime(DateTime.Now);
+            ReworkRequestId = reworkRequestId;
         }
 
         private MaterialUse() : base(Guid.NewGuid()) { }
 
-        public static MaterialUse Create(Guid materialId, Guid batchId, Guid assignId, decimal quantityDivide)
+        public static MaterialUse Create(Guid materialId, Guid batchId, Guid assignId, decimal quantityDivide, Guid? reworkRequestId)
         {
-            return new MaterialUse(Guid.NewGuid(), materialId, batchId, assignId, quantityDivide);
+            return new MaterialUse(Guid.NewGuid(), materialId, batchId, assignId, quantityDivide, reworkRequestId);
         }
 
         public void IncreaseQuantityStaffUse(int quantityProduction)
