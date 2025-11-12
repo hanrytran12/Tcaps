abafa28c045098bd272524c7b8a83f663988bd92
Trần Võ Đức Duy <tranduy1632004@gmail.com>
2025-11-12 17:37:09 +0700

    Feat(MaterialUse): Lấy danh sách NVL cho Staff

---
+++ API/Controllers/MaterialUseController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Features.MaterialUses.Query.GetMaterialUseByAssignId;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MaterialUseController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public MaterialUseController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("for-staff")]
+        public async Task<IActionResult> GetByAssignIdAsync([FromQuery] GetMaterialUseByAssignIdQuery query)
+        {
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+    }
+}
