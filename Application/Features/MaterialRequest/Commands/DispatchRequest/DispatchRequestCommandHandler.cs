eec3d9e52bcefc7aa9d7bb597bff15b8e84ae56b
hanrytran12 <85053963+hanrytran12@users.noreply.github.com>
2025-11-12 18:24:01 +0700

    feat(Notification): Send noti to QC about material from Lead

--- Application/Features/MaterialRequest/Commands/DispatchRequest/DispatchRequestCommandHandler.cs
+++ Application/Features/MaterialRequest/Commands/DispatchRequest/DispatchRequestCommandHandler.cs
@@ -1,6 +1,9 @@
 ﻿using Application.Common;
+using Application.Interfaces;
+using Domain.Events;
 using Domain.Interfaces;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Features.MaterialRequest.Commands.DispatchRequest
 {
@@ -8,11 +11,13 @@ namespace Application.Features.MaterialRequest.Commands.DispatchRequest
     {
         private readonly IBatchRepository _batchRepository;
         private readonly IMaterialRequestRepository _materialRequestRepository;
+        private readonly IAppDbContext _appDbContext;
 
-        public DispatchRequestCommandHandler(IBatchRepository batchRepository, IMaterialRequestRepository materialRequestRepository)
+        public DispatchRequestCommandHandler(IBatchRepository batchRepository, IMaterialRequestRepository materialRequestRepository, IAppDbContext appDbContext)
         {
             _batchRepository = batchRepository;
             _materialRequestRepository = materialRequestRepository;
+            _appDbContext = appDbContext;
         }
 
         public async Task<Result> Handle(DispatchRequestCommand request, CancellationToken cancellationToken)
@@ -28,6 +33,9 @@ namespace Application.Features.MaterialRequest.Commands.DispatchRequest
             {
                 var materialRequest = Domain.Entities.MaterialRequest.Create(item.MaterialId, request.UserId, batch.Id, request.AssignmentId, item.Quantity, "Yêu cầu xuất kho từ Lead");
                 await _materialRequestRepository.AddAsync(materialRequest);
+
+                var material = await _appDbContext.Materials.Where(m => m.Id == item.MaterialId).FirstOrDefaultAsync();
+                materialRequest.AddDomainEvent(new MaterialRequestAddedEvent(item.Quantity, request.AssignmentId, material.Name, material.Unit));
             }
 
             return Result.Success();
---
+++ Domain/Events/MaterialRequestAddedEvent.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+namespace Domain.Events
+{
+    public class MaterialRequestAddedEvent : INotification
+    {
+        public decimal Quantity { get; set; }
+        public Guid AssignmentId { get; set; }
+        public string MaterialName { get; set; }
+        public string UnitMaterial { get; set; }
+
+        public MaterialRequestAddedEvent(decimal quantity, Guid assignmentId, string materialName, string unitMaterial)
+        {
+            Quantity = quantity;
+            AssignmentId = assignmentId;
+            MaterialName = materialName;
+            UnitMaterial = unitMaterial;
+        }
+    }
+}
