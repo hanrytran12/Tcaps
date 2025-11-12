d1bcd25d3e62efd227f5389966d84cc79cd0ad10
huy <sliverzack1@gmail.com>
2025-11-12 20:00:18 +0700

    feat(Material): Check name is exist on db

--- Application/Features/Materials/Commands/AddMaterial/AddMaterialCommandHandler.cs
+++ Application/Features/Materials/Commands/AddMaterial/AddMaterialCommandHandler.cs
@@ -1,20 +1,31 @@
 ﻿using Application.Common;
+using Application.Interfaces;
 using Domain.Entities;
 using Domain.Interfaces;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Features.Materials.Commands.AddMaterial
 {
     public class AddMaterialCommandHandler : IRequestHandler<AddMaterialCommand, Result<Guid>>
     {
         private readonly IMaterialRepository _materialRepository;
-        public AddMaterialCommandHandler(IMaterialRepository materialRepository)
+        private readonly IAppDbContext _appDbContext;
+
+        public AddMaterialCommandHandler(IMaterialRepository materialRepository, IAppDbContext appDbContext)
         {
             _materialRepository = materialRepository;
+            _appDbContext = appDbContext;
         }
 
         public async Task<Result<Guid>> Handle(AddMaterialCommand request, CancellationToken cancellationToken)
         {
+            var materialDb = await _appDbContext.Materials.Where(m => m.Name.ToLower() == (request.Name.ToLower())).FirstOrDefaultAsync();
+            if (materialDb is not null)
+            {
+                return Result<Guid>.Failure("Tên nguyên vật liệu đã có sẵn trong hệ thống");
+            }
+
             var material = Material.Create(request.Name, request.Description, request.Unit);
             await _materialRepository.AddAsync(material);
             return Result<Guid>.Success(material.Id);
