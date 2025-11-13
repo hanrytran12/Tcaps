c16552b4b5f354a7b9552b20f4d092e60e5c5acd
hanrytran12 <85053963+hanrytran12@users.noreply.github.com>
2025-11-13 14:10:02 +0700

    fear(ReworkRequest) get reworkRequest

---
+++ Application/Features/ReworkRequest/Queries/GetRequestById/GetRequestByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Application.Features.ReworkRequest.Queries.GetRequestById
+{
+    public class GetRequestByIdQuery : IRequest<Domain.Entities.ReworkRequest>
+    {
+        public Guid ReworkRequestId { get; set; }
+
+        public GetRequestByIdQuery(Guid reworkRequestId)
+        {
+            ReworkRequestId = reworkRequestId;
+        }
+    }
+}
---
+++ Application/Features/ReworkRequest/Queries/GetRequestById/GetRequestByIdQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.ReworkRequest.Queries.GetRequestById
+{
+    public class GetRequestByIdQueryHandler : IRequestHandler<GetRequestByIdQuery, Domain.Entities.ReworkRequest>
+    {
+        private readonly IAppDbContext _appDbContext;
+
+        public GetRequestByIdQueryHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<Domain.Entities.ReworkRequest> Handle(GetRequestByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _appDbContext.ReworkRequests.Where(r => r.Id == request.ReworkRequestId).FirstOrDefaultAsync(cancellationToken);
+        }
+    }
+}
