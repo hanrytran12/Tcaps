d9795db72c9a24ae830aa70d5674a55d756b293e
huy <sliverzack1@gmail.com>
2025-11-16 22:27:05 +0700

    feat(Notification): Change message and type for batch completion

--- Application/Features/Batches/Events/BatchCompletedEventHandler.cs
+++ Application/Features/Batches/Events/BatchCompletedEventHandler.cs
@@ -1,7 +1,9 @@
-﻿using Domain.Entities;
+﻿using Application.Interfaces;
+using Domain.Entities;
 using Domain.Events;
 using Domain.Interfaces;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Features.Batches.Events
 {
@@ -9,19 +11,28 @@ namespace Application.Features.Batches.Events
     {
         private readonly INotificationRepository _notificationRepository;
         private readonly IUserRepository _userRepository;
+        private readonly IAppDbContext _appDbContext;
 
-        public BatchCompletedEventHandler(INotificationRepository notificationRepository, IUserRepository userRepository)
+        public BatchCompletedEventHandler(INotificationRepository notificationRepository, IUserRepository userRepository, IAppDbContext appDbContext)
         {
             _notificationRepository = notificationRepository;
             _userRepository = userRepository;
+            _appDbContext = appDbContext;
         }
 
         public async Task Handle(BatchCompletedEvent notification, CancellationToken cancellationToken)
         {
+            var query = from b in _appDbContext.Batches
+                        where b.Id == notification.BatchId
+                        join p in _appDbContext.Products on b.ProductId equals p.Id
+                        select new { p.Name };
+
+            var queryInfo = await query.FirstOrDefaultAsync();
+
             var user = await _userRepository.GetByRoleAsync("Admin");
             var title = $"Lô hàng {notification.BatchCode} đã hoàn thành";
-            var message = $"Lô hàng {notification.BatchCode} với ID {notification.BatchId} đã hoàn thành toàn bộ công đoạn.";
-            var type = "BatchCompletion";
+            var message = $"Lô hàng {notification.BatchCode} của {queryInfo?.Name} đã hoàn thành toàn bộ công đoạn.";
+            var type = "BATCH_COMPLETION";
 
             var result = Notification.Create(user.Id, title, message, type);
 
