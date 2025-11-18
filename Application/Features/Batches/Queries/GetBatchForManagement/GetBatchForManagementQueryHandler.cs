1e1cd1cba1d95b1891080f5e1f2dac17642eeac4
huy <sliverzack1@gmail.com>
2025-11-18 20:55:22 +0700

    feat(Batch): Get batch for management

---
+++ Application/Features/Batches/Queries/GetBatchForManagement/GetBatchForManagementQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.DTOs.Response;
+using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.Batches.Queries.GetBatchForManagement
+{
+    public class GetBatchForManagementQueryHandler : IRequestHandler<GetBatchForManagementQuery, List<BatchDTO>>
+    {
+        private readonly IAppDbContext _appDbContext;
+
+        public GetBatchForManagementQueryHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<List<BatchDTO>> Handle(GetBatchForManagementQuery request, CancellationToken cancellationToken)
+        {
+            var batchList = from b in _appDbContext.Batches
+                            join p in _appDbContext.Products on b.ProductId equals p.Id
+                            select new BatchDTO
+                            {
+                                BatchId = b.Id,
+                                Code = b.Code,
+                                ProductName = p.Name,
+                                Quantity = b.Quantity,
+                                StartDate = b.StartDate,
+                                EndDate = b.EndDate,
+                                Status = b.Status,
+                            };
+
+            return await batchList.ToListAsync();
+        }
+    }
+}
