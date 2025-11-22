82cac13ed588b3af0808ac94c9814577d9738ee6
huy <sliverzack1@gmail.com>
2025-11-23 02:19:17 +0700

    feat(Inventory): Get Inventory history for Material

---
+++ Application/Features/Inventories/Queries/GetInventoryByMaterialId/GetInventoryByMaterialIdQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using Application.DTOs.Response;
+using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.Inventories.Queries.GetInventoryByMaterialId
+{
+    public class GetInventoryByMaterialIdQueryHandler : IRequestHandler<GetInventoryByMaterialIdQuery, InventoryHistoryDTO>
+    {
+        private readonly IAppDbContext _context;
+        private readonly IFileStorageService _fileStorageService;
+
+        public GetInventoryByMaterialIdQueryHandler(IAppDbContext context, IFileStorageService fileStorageService)
+        {
+            _context = context;
+            _fileStorageService = fileStorageService;
+        }
+
+        public async Task<InventoryHistoryDTO> Handle(GetInventoryByMaterialIdQuery request, CancellationToken cancellationToken)
+        {
+            var query = _context.Inventories
+                .Where(i => i.MaterialId == request.MaterialId &&
+                            i.Date.Month == request.Month &&
+                            i.Date.Year == request.Year)
+                .Select(i => new InventoryDTO
+                {
+                    Date = i.Date,
+                    Quantity = i.Quantity,
+                    Price = i.Price,
+                    Image = _fileStorageService.GetFileUrl(i.ImageURL)
+                });
+
+            var inventoryList = await query.ToListAsync(cancellationToken);
+            return new InventoryHistoryDTO
+            {
+                TotalPrice = inventoryList.Sum(i => i.Price * i.Quantity),
+                TotalQuantity = inventoryList.Sum(i => i.Quantity),
+                Inventories = inventoryList
+            };
+        }
+    }
+}
