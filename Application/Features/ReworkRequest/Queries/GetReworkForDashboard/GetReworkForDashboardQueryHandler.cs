dcf4922a121b50fe00f57670ad1d0e677c6c6b75
huy <sliverzack1@gmail.com>
2025-11-22 19:48:37 +0700

    feat(ReworkRequest): Get reworkrRequest for dashboard

---
+++ Application/Features/ReworkRequest/Queries/GetReworkForDashboard/GetReworkForDashboardQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.ReworkRequest.Queries.GetReworkForDashboard
+{
+    public class GetReworkForDashboardQueryHandler : IRequestHandler<GetReworkForDashboardQuery, Domain.Entities.ReworkRequest>
+    {
+        private readonly IAppDbContext _appDbContext;
+
+        public GetReworkForDashboardQueryHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<Domain.Entities.ReworkRequest> Handle(GetReworkForDashboardQuery request, CancellationToken cancellationToken)
+        {
+            var reworkRequest = await _appDbContext.ReworkRequests.Where(r => r.AssignmentId == request.AssignmentId).FirstOrDefaultAsync(cancellationToken);
+            return reworkRequest;
+        }
+    }
+}
