4e92b4005e153f11d7850a4812df24914cb36194
huy <sliverzack1@gmail.com>
2025-11-23 23:52:05 +0700

    refactor(Assigment): Get detail assignment by Batch Id

--- Application/Features/Assignments/Queries/GetDetailAssignmentByBatchId/GetDetailAssignmentByBatchIdQueryHandler.cs
+++ Application/Features/Assignments/Queries/GetDetailAssignmentByBatchId/GetDetailAssignmentByBatchIdQueryHandler.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Application.Common;
+﻿using Application.Common;
 using Application.DTOs.Response;
 using Application.Interfaces;
 using MediatR;
@@ -21,18 +16,19 @@ namespace Application.Features.Assignments.Queries.GetDetailAssignmentByBatchId
         }
         public async Task<Result<List<DashboardAssignmentDTO>>> Handle(GetDetailAssignmentByBatchIdQuery request, CancellationToken cancellationToken)
         {
-            var assignments = await (from b in _context.Batches.AsNoTracking()
-                                     join a in _context.Assignments.AsNoTracking() on b.Id equals a.BatchId
-                                     join w in _context.Workshop.AsNoTracking() on a.WorkshopId equals w.Id
-                                     where b.Id == request.BatchId
-                                     select new DashboardAssignmentDTO
-                                     {
-                                         WorkshopName = w.Name,
-                                         Quantity = a.Quantity,
-                                         StartDate = a.StartDate,
-                                         EndDate = a.EndDate,
-                                         Status = a.Status
-                                     }).ToListAsync(cancellationToken);
+            var assignments = await (
+                from a in _context.Assignments.AsNoTracking()
+                where a.BatchId == request.BatchId
+                join w in _context.Workshop.AsNoTracking() on a.WorkshopId equals w.Id
+                orderby a.StepOrder
+                select new DashboardAssignmentDTO
+                {
+                    WorkshopName = w.Name,
+                    Quantity = a.Quantity,
+                    StartDate = a.StartDate,
+                    EndDate = a.EndDate,
+                    Status = a.Status,
+                }).ToListAsync(cancellationToken);
 
             return Result<List<DashboardAssignmentDTO>>.Success(assignments);
         }
