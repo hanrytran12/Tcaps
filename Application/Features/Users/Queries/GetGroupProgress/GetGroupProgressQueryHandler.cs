c9a79291875e3a26145c68ce31670f9d8d1dd50b
Trần Võ Đức Duy <tranduy1632004@gmail.com>
2025-11-23 19:04:57 +0700

    Chỉnh lại quá trình làm việc của nhóm có thêm Reworking

--- Application/Features/Users/Queries/GetGroupProgress/GetGroupProgressQueryHandler.cs
+++ Application/Features/Users/Queries/GetGroupProgress/GetGroupProgressQueryHandler.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Application.Common;
 using Application.DTOs.Response;
 using Application.Interfaces;
+using Domain.Entities;
 using Domain.Interfaces;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -31,8 +32,14 @@ namespace Application.Features.Users.Queries.GetGroupProgress
             // 🔹 Lấy assignment đang hoạt động
             var assignment = await _context.Assignments.FindAsync(request.AssignId);
 
+            var reworkRequest = assignment.Status == "Reworking" ?
+                await _context.ReworkRequests
+                .Where(r => r.AssignmentId == assignment.Id
+                    && r.Status == "Approved")
+                .FirstOrDefaultAsync(cancellationToken) : null;
+
             var today = DateOnly.FromDateTime(DateTime.UtcNow);
-            
+
             // Tổng sản lượng nhóm cho assignment này
             var totalProduction = await _context.Productions
                 .Where(p => p.AssignId == request.AssignId)
@@ -40,41 +47,57 @@ namespace Application.Features.Users.Queries.GetGroupProgress
 
             // 🔹 Tính tổng sản phẩm Unfixable của toàn nhóm trong assignment này
             var totalUnfixable = await (from a in _context.Assignments
-                                 join p in _context.Productions on a.Id equals p.AssignId
-                                 join e in _context.Evaluates on p.Id equals e.ProductionId
-                                 join c in _context.ComponentDefects on e.Id equals c.EvaluateId
-                                 where p.AssignId == request.AssignId && c.Status == "Unfixable"
-                                 select c.Quantity).SumAsync(cancellationToken);
+                                        join p in _context.Productions on a.Id equals p.AssignId
+                                        join e in _context.Evaluates on p.Id equals e.ProductionId
+                                        join c in _context.ComponentDefects on e.Id equals c.EvaluateId
+                                        where p.AssignId == request.AssignId && c.Status == "Unfixable"
+                                        select c.Quantity).SumAsync(cancellationToken);
 
             // Tiến độ
             var target = assignment.Quantity; // 100
+
+            var productionRework = 0;
+            var targetRework = 0;
+            var remainingRework = 0;
+
+            if (assignment.Status == "Reworking" && reworkRequest != null)
+            {
+                targetRework = (int)reworkRequest.DefectiveQuantity;
+                productionRework = Math.Max(totalProduction - target, 0);
+                remainingRework = Math.Max(targetRework - productionRework, 0);
+            }
+
             //var effectiveProduction = totalProduction - totalUnfixable; //105 - 5 = 100
             var remaining = Math.Max(target - totalProduction, 0); // 100 - 100
 
             // Số ngày còn lại (dùng DateOnly)
-            var daysLeft = 0;
-            if (assignment.EndDate != default)
+            var endDate = assignment.EndDate;
+            if (assignment.Status == "Reworking" && reworkRequest?.EndDate != default)
             {
-                daysLeft = Math.Max(assignment.EndDate.DayNumber - today.DayNumber, 0);
+                endDate = reworkRequest.EndDate.Value;
             }
+            var daysLeft = endDate != default ? Math.Max(endDate.DayNumber - today.DayNumber, 0) : 0;
 
             // Lấy danh sách nhân viên tham gia
             var staffNames = await (from p in _context.Productions
-                               join u in _context.Users on p.UserId equals u.Id
-                               where p.AssignId == assignment.Id
-                               select u.FullName)
+                                    join u in _context.Users on p.UserId equals u.Id
+                                    where p.AssignId == assignment.Id
+                                    select u.FullName)
                                .Distinct()
                                .ToListAsync(cancellationToken);
 
             var results = new GroupProgressDTO
             {
-                 WorkshopName = workshop.Name,
-                 Target = target,
-                 CurrentProduction = totalProduction,
-                 TotalUnfixable = totalUnfixable,
-                 RemainingProducts = remaining,
-                 DaysLeft = daysLeft,
-                 Members = staffNames
+                WorkshopName = workshop.Name,
+                Target = target,
+                CurrentProduction = totalProduction,
+                TotalUnfixable = totalUnfixable,
+                RemainingProducts = remaining,
+                TargetRework = targetRework,
+                ReworkProduction = productionRework,
+                RemainingRework = remainingRework,
+                DaysLeft = daysLeft,
+                Members = staffNames //cuong, dung
             };
 
             return Result<GroupProgressDTO>.Success(results);
