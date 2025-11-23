99831d8eeada7642ba70b25026d999ffcb161cd7
Trần Võ Đức Duy <tranduy1632004@gmail.com>
2025-11-24 01:00:52 +0700

    Feat(Workshop): Add workshop

---
+++ Application/Features/Workshop/Command/AddWorkshop/AddWorkshopCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Common;
+using MediatR;
+
+namespace Application.Features.Workshop.Command.AddWorkshop
+{
+    public class AddWorkshopCommand : IRequest<Result<Guid>>
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int StepOrder { get; set; }
+    }
+}
--- Domain/Entities/Workshop.cs
+++ Domain/Entities/Workshop.cs
@@ -8,11 +8,17 @@ namespace Domain.Entities
         public string Description { get; private set; } = string.Empty;
         public int StepOrder { get; private set; }
 
-        public Workshop(Guid id, string name, string description)
+        public Workshop(Guid id, string name, string description, int stepOrder)
             : base(id)
         {
             Name = name;
             Description = description;
+            StepOrder = stepOrder;
+        }
+
+        public static Workshop Create(string name, string description, int stepOrder)
+        {
+            return new Workshop(Guid.NewGuid(), name, description, stepOrder);
         }
 
         private Workshop() : base(Guid.NewGuid()) { }
