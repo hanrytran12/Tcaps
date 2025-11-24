a4c104104992beee88d8e324b8778e9cd784f7f0
huy <sliverzack1@gmail.com>
2025-11-24 23:53:04 +0700

    refactor(Material): Show quantity send to dashboard Inventory

--- Application/DTOs/Response/MaterialToWatchDTO.cs
+++ Application/DTOs/Response/MaterialToWatchDTO.cs
@@ -8,5 +8,6 @@
         public int Quantity { get; set; }
         public string Unit { get; set; } = string.Empty;
         public decimal Price { get; set; }
+        public int QuantitySend { get; set; }
     }
 }
