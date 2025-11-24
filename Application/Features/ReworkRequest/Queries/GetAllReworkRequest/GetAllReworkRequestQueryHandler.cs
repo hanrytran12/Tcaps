c1862ad1e03d8e6d4b5e50b9d67cd61a5aefdb0a
huy <sliverzack1@gmail.com>
2025-11-25 02:50:07 +0700

    refactor(ReworkRequest): Get more information from ReworkRequest

--- Application/Features/ReworkRequest/Queries/GetAllReworkRequest/GetAllReworkRequestQueryHandler.cs
+++ Application/Features/ReworkRequest/Queries/GetAllReworkRequest/GetAllReworkRequestQueryHandler.cs
@@ -1,10 +1,11 @@
-﻿using Application.Interfaces;
+﻿using Application.DTOs.Response;
+using Application.Interfaces;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace Application.Features.ReworkRequest.Queries.GetAllReworkRequest
 {
-    public class GetAllReworkRequestQueryHandler : IRequestHandler<GetAllReworkRequestQuery, List<Domain.Entities.ReworkRequest>>
+    public class GetAllReworkRequestQueryHandler : IRequestHandler<GetAllReworkRequestQuery, List<ReworkRequestDTO>>
     {
         private readonly IAppDbContext _appDbContext;
 
@@ -13,9 +14,28 @@ namespace Application.Features.ReworkRequest.Queries.GetAllReworkRequest
             _appDbContext = appDbContext;
         }
 
-        public async Task<List<Domain.Entities.ReworkRequest>> Handle(GetAllReworkRequestQuery request, CancellationToken cancellationToken)
+        public async Task<List<ReworkRequestDTO>> Handle(GetAllReworkRequestQuery request, CancellationToken cancellationToken)
         {
-            return await _appDbContext.ReworkRequests.ToListAsync();
+            var query = from rr in _appDbContext.ReworkRequests
+                        join a in _appDbContext.Assignments on rr.AssignmentId equals a.Id
+                        join w in _appDbContext.Workshop on a.WorkshopId equals w.Id
+                        join b in _appDbContext.Batches on a.BatchId equals b.Id
+                        join u in _appDbContext.Users on rr.QcId equals u.Id
+                        select new ReworkRequestDTO
+                        {
+                            BatchCode = b.Code,
+                            QcName = u.FullName,
+                            WorkshopName = w.Name,
+                            DefectiveQuantity = rr.DefectiveQuantity,
+                            NoteQc = rr.NoteQc,
+                            Status = rr.Status,
+                            CreatedAt = rr.CreatedAt,
+                            DeliveryDate = rr.DeliveryDate,
+                            EndDate = rr.EndDate,
+                            NextStepDeliveryDate = rr.NextStepDeliveryDate
+                        };
+
+            return await query.ToListAsync();
         }
     }
 }
