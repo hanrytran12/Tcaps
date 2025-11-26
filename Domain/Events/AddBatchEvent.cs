e834f95302a8bcc14b6ba6628ac3b4a94d855160
Trần Võ Đức Duy <tranduy1632004@gmail.com>
2025-11-26 23:39:16 +0700

    Feat(Notification): Thêm thông báo Admin tạo lô cho Lead

---
+++ Domain/Events/AddBatchEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Domain.Events
+{
+    public class AddBatchEvent : INotification
+    {
+        public string BatchCode { get; set; }
+        public decimal Quantity { get; set; }
+        public AddBatchEvent(string batchCode, int quantity)
+        {
+            BatchCode = batchCode;
+            Quantity = quantity;
+        }
+    }
+}
