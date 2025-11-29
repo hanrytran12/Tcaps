23264f6e3eac148006e402ca6deaeaa5c3a44ab8
huy <sliverzack1@gmail.com>
2025-11-29 22:03:04 +0700

    feat(User): User can re-active

---
+++ Application/Features/Users/Commands/ReactiveUser/ReactiveUserCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.Common;
+using Domain.Interfaces;
+using MediatR;
+
+namespace Application.Features.Users.Commands.ReactiveUser
+{
+    public class ReactiveUserCommandHandler : IRequestHandler<ReactiveUserCommand, Result>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public ReactiveUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<Result> Handle(ReactiveUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(request.UserId);
+            if (user is null)
+            {
+                return Result.Failure("User not found.");
+            }
+
+            user.Reactive();
+            return Result.Success();
+        }
+    }
+}
