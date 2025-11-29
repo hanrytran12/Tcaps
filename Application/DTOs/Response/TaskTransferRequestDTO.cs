da68910c1ebb46159e649273052a02f632780906
Trần Võ Đức Duy <tranduy1632004@gmail.com>
2025-11-30 04:56:35 +0700

    Thêm MaterialName và QuantityRequest

--- Application/DTOs/Response/TaskTransferRequestDTO.cs
+++ Application/DTOs/Response/TaskTransferRequestDTO.cs
@@ -17,6 +17,8 @@ namespace Application.DTOs.Response
         public string QcTransportName { get; set; }
         public Guid? MaterialRequestId { get; set; }
         public Guid? AssignmentTransferId { get; set; }
+        public string MaterialName { get; set; }
+        public int QuantityRequest { get; set; }
         public string Status { get; set; }
         public string? Note { get; set; }
         public DateTime CreatedAt { get; set; }
