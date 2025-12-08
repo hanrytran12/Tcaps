3fcbac5a6f2c4aeeeab18c6f80d5f65ef45c08ea
huy <sliverzack1@gmail.com>
2025-12-08 16:11:55 +0700

    refactor(WorkshopController): Refactor API of Workshop

--- Application/Features/Workshop/Command/AddWorkshop/AddWorkshopCommand.cs
+++ Application/Features/Workshop/Command/AddWorkshop/AddWorkshopCommand.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Application.Common;
+﻿using Application.Common;
 using MediatR;
 
 namespace Application.Features.Workshop.Command.AddWorkshop
