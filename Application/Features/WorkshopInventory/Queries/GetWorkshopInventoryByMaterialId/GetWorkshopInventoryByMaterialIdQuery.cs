5a0540d6844ab83220f12206ee7a5c2296f6fb06
huy <sliverzack1@gmail.com>
2025-12-08 16:12:27 +0700

    refactor(WorkshopInventoryController): Refactor API of WorkshopInventory

--- Application/Features/WorkshopInventory/Queries/GetWorkshopInventoryByMaterialId/GetWorkshopInventoryByMaterialIdQuery.cs
+++ Application/Features/WorkshopInventory/Queries/GetWorkshopInventoryByMaterialId/GetWorkshopInventoryByMaterialIdQuery.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Application.Common;
-using Application.DTOs.Response;
-using MediatR;
+﻿using MediatR;
 
 namespace Application.Features.WorkshopInventory.Queries.GetWorkshopInventoryByMaterialId
 {
-    public class GetWorkshopInventoryByMaterialIdQuery : IRequest<Result<Domain.Entities.WorkshopInventory>>
+    public class GetWorkshopInventoryByMaterialIdQuery : IRequest<Domain.Entities.WorkshopInventory>
     {
         public Guid MaterialId { get; set; }
     }
