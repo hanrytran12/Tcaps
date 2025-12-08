2a9dee7a49cdacb0562088b5683c2b328d9b90f1
huy <sliverzack1@gmail.com>
2025-12-08 17:52:46 +0700

    refactor(UserController): Refactor API of User

--- API/Controllers/QCController.cs
+++ API/Controllers/QCController.cs
@@ -1,38 +1,24 @@
-﻿using Application.Features.ComponentDefects.Query.GetComponentDefects;
-using MediatR;
+﻿using Application.DTOs.Response;
+using Application.Features.ComponentDefects.Query.GetComponentDefects;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class QCController : ControllerBase
+    public class QCController : BaseApiController
     {
-        private readonly IMediator _mediator;
-        public QCController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
         [HttpGet("rework-requests")]
         [Authorize(Policy = "QC")]
-        public async Task<IActionResult> GetAllComponentDefect([FromQuery] string? status)
+        public async Task<List<ComponentDefectsDTO>> GetAllComponentDefect([FromQuery] string? status)
         {
-            var userIdString = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
-            if (string.IsNullOrEmpty(userIdString))
-            {
-                return Unauthorized("Không thể xác định người dùng từ token.");
-            }
-
-            var userId = Guid.Parse(userIdString);
             var query = new GetComponentDefectsQuery();
-            query.QCId = userId;
-            query.Status = status;
-            var result = await _mediator.Send(query);
-            return Ok(result);
+            return await Mediator.Send(new GetComponentDefectsQuery
+            {
+                QCId = CurrentUserId,
+                Status = status
+            });
         }
     }
 }
