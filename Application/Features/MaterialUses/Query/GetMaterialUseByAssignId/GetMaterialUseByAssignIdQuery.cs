63623fc0ba3b467e4c4e1259da81b13f3f3cd637
huy <sliverzack1@gmail.com>
2025-12-08 23:33:00 +0700

    refactor(MaterialUse): refactor API of materialUse

--- Application/Features/MaterialUses/Query/GetMaterialUseByAssignId/GetMaterialUseByAssignIdQuery.cs
+++ Application/Features/MaterialUses/Query/GetMaterialUseByAssignId/GetMaterialUseByAssignIdQuery.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Application.Common;
-using Application.DTOs.Response;
+﻿using Application.DTOs.Response;
 using MediatR;
 
 namespace Application.Features.MaterialUses.Query.GetMaterialUseByAssignId
 {
-    public class GetMaterialUseByAssignIdQuery : IRequest<Result<List<MaterialUseDTO>>>
+    public class GetMaterialUseByAssignIdQuery : IRequest<List<MaterialUseDTO>>
     {
         public Guid AssignId { get; set; }
         public string? MaterialName { get; set; }
