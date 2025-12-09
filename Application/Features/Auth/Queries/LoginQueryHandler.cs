8e9511947f8b7af74472c75b1544d673b44e0282
huy <sliverzack1@gmail.com>
2025-12-09 13:21:57 +0700

    refactor(Auth): Fix response API

--- Application/Features/Auth/Queries/LoginQueryHandler.cs
+++ Application/Features/Auth/Queries/LoginQueryHandler.cs
@@ -1,4 +1,4 @@
-﻿using Application.Common;
+﻿using Application.Common.Exceptions;
 using Application.DTOs.Response;
 using Application.Interfaces;
 using Domain.Interfaces;
@@ -6,7 +6,7 @@ using MediatR;
 
 namespace Application.Features.Auth.Queries
 {
-    public class LoginQueryHandler : IRequestHandler<LoginQuery, Result<AuthRepsponseDTO>>
+    public class LoginQueryHandler : IRequestHandler<LoginQuery, AuthRepsponseDTO>
     {
         private readonly IUserRepository _userRepository;
         private readonly IJwtTokenGenerator _jwtTokenGenerator;
@@ -17,23 +17,27 @@ namespace Application.Features.Auth.Queries
             _jwtTokenGenerator = jwtTokenGenerator;
         }
 
-        public async Task<Result<AuthRepsponseDTO>> Handle(LoginQuery request, CancellationToken cancellationToken)
+        public async Task<AuthRepsponseDTO> Handle(LoginQuery request, CancellationToken cancellationToken)
         {
             var user = await _userRepository.FindByEmailOrPhoneAsync(request.EmailOrPhone);
 
             if (user is null)
             {
-                return Result<AuthRepsponseDTO>.Failure("Thông tin đăng nhập không chính xác.");
+                throw new NotFoundException("Thông tin đăng nhập không chính xác.");
             }
 
             bool isPasswordVaid = BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash);
             if (!isPasswordVaid)
             {
-                return Result<AuthRepsponseDTO>.Failure("Wrong password.");
+                throw new ConflictException("Wrong password.");
             }
 
             var tokenResponse = _jwtTokenGenerator.GenerateToken(user);
-            return Result<AuthRepsponseDTO>.Success(tokenResponse);
+            return new AuthRepsponseDTO
+            {
+                Token = tokenResponse.Token,
+                ExpiresAt = tokenResponse.ExpiresAt,
+            };
         }
     }
 }
