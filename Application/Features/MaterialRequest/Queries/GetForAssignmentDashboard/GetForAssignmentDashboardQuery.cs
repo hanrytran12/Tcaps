d4171e000ded7285992fed6deae5f6c02389319d
Trần Võ Đức Duy <tranduy1632004@gmail.com>
2025-12-19 17:21:30 +0700

    feat(MaterialRequest): Add query for assignment dashboard material requests

---
+++ Application/Features/MaterialRequest/Queries/GetForAssignmentDashboard/GetForAssignmentDashboardQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.DTOs.Response;
+using MediatR;
+
+namespace Application.Features.MaterialRequest.Queries.GetForAssignmentDashboard
+{
+    public class GetForAssignmentDashboardQuery : IRequest<List<MaterialRequestForAssignmentDashboardDTO>>
+    {
+        public Guid AssignmentId { get; set; }
+    }
+}
