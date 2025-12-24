c0a974b0c7813f2c3a2d611627118ea09c9ce1f7
Trần Võ Đức Duy <tranduy1632004@gmail.com>
2025-12-24 15:34:28 +0700

    feat: Implement GetAllProductionByAssignId query and handler, and add corresponding API endpoint

---
+++ Application/Features/Productions/Query/GetAllProductionByAssignId/GetAllProductionByAssignIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.DTOs.Response;
+using MediatR;
+
+namespace Application.Features.Productions.Query.GetAllProductionByAssignId
+{
+    public class GetAllProductionByAssignIdQuery : IRequest<List<ProductionDTO>>
+    {
+        public Guid AssignId { get; set; }
+    }
+}
---
+++ Application/Features/Productions/Query/GetAllProductionByAssignId/GetAllProductionByAssignIdQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.DTOs.Response;
+using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.Productions.Query.GetAllProductionByAssignId
+{
+    public class GetAllProductionByAssignIdQueryHandler : IRequestHandler<GetAllProductionByAssignIdQuery, List<ProductionDTO>>
+    {
+        private readonly IAppDbContext _context;
+
+        public GetAllProductionByAssignIdQueryHandler(IAppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<ProductionDTO>> Handle(GetAllProductionByAssignIdQuery request, CancellationToken cancellationToken)
+        {
+            var productions = await (from pro in _context.Productions
+                                     join u in _context.Users on pro.UserId equals u.Id
+                                     where pro.AssignId == request.AssignId
+                                     select new ProductionDTO
+                                     {
+                                         Id = pro.Id,
+                                         AssignId = request.AssignId,
+                                         UserId = u.Id,
+                                         FullName = u.FullName,
+                                         Quantity = pro.Quantity,
+                                         Date = pro.Date,
+                                         Time = pro.Time,
+                                         Status = pro.Status
+                                     })
+                                     .ToListAsync();
+
+            return productions;
+        }
+    }
+}
