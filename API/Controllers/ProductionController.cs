3f90f2b3fad5ed67ca42e5db77b5a063ae8155c2
Trần Võ Đức Duy <tranduy1632004@gmail.com>
2025-12-24 15:34:28 +0700

    feat: Implement GetAllProductionByAssignId query and handler, and add corresponding API endpoint

--- API/Controllers/ProductionController.cs
+++ API/Controllers/ProductionController.cs
@@ -2,6 +2,7 @@
 using Application.Features.Productions.Command.AddProductionReport;
 using Application.Features.Productions.Command.UpdateProduction;
 using Application.Features.Productions.Query.GetAllProduction;
+using Application.Features.Productions.Query.GetAllProductionByAssignId;
 using Application.Features.Productions.Query.GetAllProductionByQCId;
 using Application.Features.Productions.Query.GetAllProductionByStaffId;
 using Microsoft.AspNetCore.Authorization;
@@ -37,6 +38,12 @@ namespace API.Controllers
                 QC_Id = CurrentUserId,
                 Status = status
             });
+        }
+
+        [HttpGet("by-assignId")]
+        public async Task<List<ProductionDTO>> GetProductionByAssignIdAsync([FromQuery] GetAllProductionByAssignIdQuery query)
+        {
+            return await Mediator.Send(query);
         }
 
         [HttpPost("report-work")]
--- Application/DTOs/Response/ProductionDTO.cs
+++ Application/DTOs/Response/ProductionDTO.cs
@@ -10,7 +10,7 @@ namespace Application.DTOs.Response
     {
         public Guid Id { get; set; }        // thêm nếu dùng để submit lại
         public Guid AssignId { get; set; }
-        public string BatchCode { get; set; }
+        public string BatchCode { get; set; } = string.Empty;
         public Guid UserId { get; set; }
         public string? FullName { get; set; }
         public int Quantity { get; set; }
