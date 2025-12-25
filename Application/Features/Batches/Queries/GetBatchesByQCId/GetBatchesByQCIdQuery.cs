51930eb8a88e1ce8dc8f66312f6a52b90bd8d632
Trần Võ Đức Duy <tranduy1632004@gmail.com>
2025-12-25 10:47:46 +0700

    refactor(GetBatchesByQCId): change return type to List<BatchForQCDTO> for batch retrieval

--- Application/Features/Batches/Queries/GetBatchesByQCId/GetBatchesByQCIdQuery.cs
+++ Application/Features/Batches/Queries/GetBatchesByQCId/GetBatchesByQCIdQuery.cs
@@ -3,7 +3,7 @@ using MediatR;
 
 namespace Application.Features.Batches.Queries.GetBatchesByQCId
 {
-    public class GetBatchesByQCIdQuery : IRequest<BatchForQCDTO>
+    public class GetBatchesByQCIdQuery : IRequest<List<BatchForQCDTO>>
     {
         public Guid QcId { get; set; }
     }
