72c0af5c4d1e0159d1eff04c3b0a2e136ea63fde
Trần Võ Đức Duy <tranduy1632004@gmail.com>
2025-12-29 17:42:56 +0700

    feat: add notification system for staff on material request confirmation

---
+++ Domain/Events/NotificationForStaffEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Domain.Events
+{
+    public class NotificationForStaffEvent : INotification
+    {
+        public Guid UserId { get; set; }
+        public Guid BatchId { get; set; }
+        public decimal? ActualReceivedQuantity { get; set; }
+
+        public NotificationForStaffEvent(Guid userId, Guid batchId, decimal? actualReceivedQuantity)
+        {
+            UserId = userId;
+            BatchId = batchId;
+            ActualReceivedQuantity = actualReceivedQuantity;
+        }
+    }
+}
