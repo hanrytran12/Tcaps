5e67db01d73cebdea36c2788023286d927ca4ceb
huy <sliverzack1@gmail.com>
2026-01-30 09:00:23 +0700

    refactor: Add signalR for Admin about Batch complete

--- Infrastructure/Services/NotificationServices.cs
+++ Infrastructure/Services/NotificationServices.cs
@@ -590,6 +590,15 @@ namespace Infrastructure.Services
             var notification = Notification.Create(admin.Id, title, message, type);
             await _notificationRepository.AddAsync(notification);
             await _unitOfWork.SaveChangesAsync();
+
+            await _hubContext.Clients.User(admin.Id.ToString()).SendAsync("ReceiveNotification", new
+            {
+                Id = notification.Id,
+                Title = title,
+                Message = message,
+                Type = type,
+                CreatedAt = DateTime.Now
+            });
         }
 
         public async Task UpdateQuantityDefectNotificationAsync(decimal quantityReject, Guid qcId, string batchCode)
