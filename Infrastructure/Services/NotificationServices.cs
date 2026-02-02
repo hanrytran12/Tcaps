766fa9e361f116a7ca04d90a5d7d21d29eaa27c9
huy <sliverzack1@gmail.com>
2026-02-02 23:39:27 +0700

    feat: Add realtime of noti from QC to Staff

--- Infrastructure/Services/NotificationServices.cs
+++ Infrastructure/Services/NotificationServices.cs
@@ -800,6 +800,15 @@ namespace Infrastructure.Services
             {
                 var notification = Notification.Create(item.Id, title, message, type);
                 await _notificationRepository.AddAsync(notification);
+
+                await _hubContext.Clients.User(item.Id.ToString()).SendAsync("ReceiveNotification", new
+                {
+                    Id = notification.Id,
+                    Title = title,
+                    Message = message,
+                    Type = type,
+                    CreatedAt = DateTime.Now
+                });
             }
 
             await _unitOfWork.SaveChangesAsync();
