70b4a6b869061f50af8dc0f2d2b876db5ed170d0
huy <sliverzack1@gmail.com>
2026-02-08 16:51:13 +0700

    feat: Add singalR

--- Infrastructure/Services/NotificationServices.cs
+++ Infrastructure/Services/NotificationServices.cs
@@ -601,6 +601,24 @@ namespace Infrastructure.Services
             var notificationAdmin = Notification.Create(admin.Id, title, message, type);
             await _notificationRepository.AddAsync(notificationAdmin);
             await _unitOfWork.SaveChangesAsync();
+
+            await _hubContext.Clients.User(lead.Id.ToString()).SendAsync("ReceiveNotification", new
+            {
+                Id = notificationLead.Id,
+                Title = title,
+                Message = message,
+                Type = type,
+                CreatedAt = DateTime.Now
+            });
+
+            await _hubContext.Clients.User(admin.Id.ToString()).SendAsync("ReceiveNotification", new
+            {
+                Id = notificationAdmin.Id,
+                Title = title,
+                Message = message,
+                Type = type,
+                CreatedAt = DateTime.Now
+            });
         }
 
         public async Task SendQCTransportReceptionMaterialRequestNotificationAsync(Guid qcTransportId, Guid materialRequestId, Guid assignId)
@@ -632,6 +650,24 @@ namespace Infrastructure.Services
             var notificationAdmin = Notification.Create(admin.Id, title, message, type);
             await _notificationRepository.AddAsync(notificationAdmin);
             await _unitOfWork.SaveChangesAsync();
+
+            await _hubContext.Clients.User(lead.Id.ToString()).SendAsync("ReceiveNotification", new
+            {
+                Id = notificationLead.Id,
+                Title = title,
+                Message = message,
+                Type = type,
+                CreatedAt = DateTime.Now
+            });
+
+            await _hubContext.Clients.User(admin.Id.ToString()).SendAsync("ReceiveNotification", new
+            {
+                Id = notificationAdmin.Id,
+                Title = title,
+                Message = message,
+                Type = type,
+                CreatedAt = DateTime.Now
+            });
         }
 
         public async Task SendConfirmRequestFromLeadNotificationAsync(Guid materialRequestId, Guid qcId)
@@ -1053,3 +1089,4 @@ namespace Infrastructure.Services
         }
     }
 }
+
