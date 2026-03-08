11cf9865c8fa8f19725aaf8f3a102faf9bc4d772
Duy163 <tranduy1632004@gmail.com>
2026-03-08 16:33:05 +0700

    feat: Add validation to prevent CompleteQuantityReceive exceeding CompletedQuantitySend in UpdateAssignmentTransferRequestCommandHandler

--- Application/Features/AssingmentTransferRequest/Commands/UpdateAssignmentTransferRequest/UpdateAssignmentTransferRequestCommandHanler.cs
+++ Application/Features/AssingmentTransferRequest/Commands/UpdateAssignmentTransferRequest/UpdateAssignmentTransferRequestCommandHanler.cs
@@ -44,6 +44,11 @@ namespace Application.Features.AssingmentTransferRequest.Commands.UpdateAssignme
                     throw new BadRequestException("Yêu cầu không hợp lệ hoặc đã được duyệt");
                 }
 
+                if (request.CompleteQuantityReceive > transferRequest.CompletedQuantitySend)
+                {
+                    throw new BadRequestException($"Số lượng thực nhận ({request.CompleteQuantityReceive}) không được lớn hơn số lượng QC đưa lên ({(int)transferRequest.CompletedQuantitySend}).");
+                }
+
                 var assigment = await _assignmentRepository.GetByIdAsync(transferRequest.AssignmentId);
                 if (assigment is null)
                 {
@@ -76,6 +81,11 @@ namespace Application.Features.AssingmentTransferRequest.Commands.UpdateAssignme
             }
             else
             {
+                if (request.CompleteQuantityReceive > transferRequest.CompletedQuantitySend)
+                {
+                    throw new BadRequestException($"Số lượng thực nhận ({request.CompleteQuantityReceive}) không được lớn hơn số lượng QC đưa lên ({(int)transferRequest.CompletedQuantitySend}).");
+                }
+
                 var reworkRequest = await _appDbContext.ReworkRequests.Where(r => r.Id == transferRequest.ReworkRequestId).FirstOrDefaultAsync();
                 reworkRequest.Completed();
 
